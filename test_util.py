0d8b63de80fe11e23662d136030333fe1888449e
Alec Deason <alecwd@uw.edu>
2016-05-19 17:12:33 -0700

    Tests for sort_modules

---
+++ test_util.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from framework import SimulationModule
+from util import sort_modules
+
+class AModule(SimulationModule):
+    pass
+class BModule(SimulationModule):
+    DEPENDENCIES = (AModule,)
+class CModule(SimulationModule):
+    DEPENDENCIES = (BModule,)
+class DModule(SimulationModule):
+    DEPENDENCIES = (CModule, BModule)
+
+class TestSortModules(TestCase):
+    def test_basic_function(self):
+        modules = {DModule: DModule(), CModule: CModule(), BModule:BModule(), AModule:AModule()}
+        sorted_modules = sort_modules(modules)
+        self.assertListEqual(sorted_modules, [modules[AModule], modules[BModule], modules[CModule], modules[DModule]])
