e4dedb402b195d0d3bee81987ff012fc36fcf15b
Alec Deason <alecwd@uw.edu>
2016-05-25 18:16:02 -0700

    Make tests pass in python2

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+import numpy as np
+
 from ceam.engine import EventHandler
 
 class TestEventHandler(TestCase):
@@ -8,15 +10,15 @@ class TestEventHandler(TestCase):
         trigger = [False]
         def listener(label, mask, simulation):
             trigger[0] = True
-            self.assertListEqual(mask, [True, False])
+            self.assertListEqual(list(mask), [True, False])
             self.assertEqual(simulation, 'mock_simulation')
         eh.register_event_listener(listener, 'test')
-        eh.emit_event('test', [True, False], 'mock_simulation')
+        eh.emit_event('test', np.array([True, False]), 'mock_simulation')
         self.assertTrue(trigger[0])
 
         trigger[0] = False
         eh.deregister_event_listener(listener, 'test')
-        eh.emit_event('test', [], None)
+        eh.emit_event('test', np.array([]), None)
         self.assertFalse(trigger[0])
     
     def test_generic_listener(self):
@@ -24,11 +26,11 @@ class TestEventHandler(TestCase):
         records = []
         eh.register_event_listener(lambda a,b,c: records.append('label'), 'test')
         eh.register_event_listener(lambda a,b,c: records.append('generic'))
-        eh.emit_event('test', [], None)
+        eh.emit_event('test', np.array([]), None)
         self.assertListEqual(records, ['label', 'generic'])
 
         records = []
-        eh.emit_event('other_test', [], None)
+        eh.emit_event('other_test', np.array([]), None)
         self.assertListEqual(records, ['generic'])
 
     def test_listener_priority(self):
@@ -37,5 +39,5 @@ class TestEventHandler(TestCase):
         eh.register_event_listener(lambda a,b,c: records.append('second'), 'test', priority=1)
         eh.register_event_listener(lambda a,b,c: records.append('first'), 'test', priority=0)
         eh.register_event_listener(lambda a,b,c: records.append('last'), 'test', priority=9)
-        eh.emit_event('test', [], None)
+        eh.emit_event('test', np.array([]), None)
         self.assertListEqual(records, ['first', 'second', 'last'])
