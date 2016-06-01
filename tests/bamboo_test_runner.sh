72ba133bbea966662272a2f1884472b09dd65b1e
Alec Deason <alecwd@uw.edu>
2016-06-01 15:33:48 -0700

    Getting things workking in an automated test environment

---
+++ tests/bamboo_test_runner.sh
@@ -0,0 +1,12 @@
+cd /home/j/Project/Cost_Effectiveness/dev/test_tmp
+source test_environment/bin/activate
+git clone ssh://git@stash.ihme.washington.edu:7999/cste/ceam.git
+cd ceam
+python setup.py develop
+git checkout "${bamboo.planRepository.revision}"
+git branch
+py.test
+e=$?
+cd /home/j/Project/Cost_Effectiveness/dev/test_tmp
+rm -rf ceam
+exit $e
