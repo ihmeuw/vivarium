9f3fb8b9ebd7197de06796e13b9f7bb40cfff683
Alec Deason <alecwd@uw.edu>
2016-06-03 15:55:31 -0700

    four drug intervention based on Tiago's docs

--- experiment_runners/opportunistic_screening.py
+++ experiment_runners/opportunistic_screening.py
@@ -18,6 +18,30 @@ from ceam.modules.metrics import MetricsModule
 
 pd.set_option('mode.chained_assignment', 'raise')
 
+#TODO: This feels like configuration but is difficult to express in ini type files
+MEDICATIONS = [
+        {
+            'name': 'Thiazide-type diuretics',
+            'daily_cost': 0.009,
+            'efficacy': 8.8,
+        },
+        {
+            'name': 'Calcium-channel blockers',
+            'daily_cost': 0.166,
+            'efficacy': 8.8,
+        },
+        {
+            'name': 'ACE Inhibitors',
+            'daily_cost': 0.059,
+            'efficacy': 10.3,
+        },
+        {
+            'name': 'Beta blockers',
+            'daily_cost': 0.048,
+            'efficacy': 9.2,
+        },
+]
+
 
 def _hypertensive_categories(population):
         under_60 = population.age < 60
@@ -49,13 +73,10 @@ class OpportunisticScreeningModule(SimulationModule):
 
     def load_population_columns(self, path_prefix, population_size):
         #TODO: Some people will start out taking medications?
-        self.population_columns = pd.DataFrame({
-            'taking_blood_pressure_medication_a': [False]*population_size,
-            'taking_blood_pressure_medication_b': [False]*population_size,
-            })
+        self.population_columns['medication_count'] = [0]*population_size
 
     def non_followup_blood_pressure_test(self, event):
-        self.cost_by_year[self.simulation.current_time.year] += len(event.affected_population) * 3.0
+        self.cost_by_year[self.simulation.current_time.year] += len(event.affected_population) * 9.73
 
         #TODO: testing error
 
@@ -67,54 +88,45 @@ class OpportunisticScreeningModule(SimulationModule):
         # Hypertensive simulants get a 1 month followup and no drugs
         self.simulation.population.loc[hypertensive.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5) # 1 month
 
-        # Severe hypertensive simulants get a 1 month followup and all drugs
+        # Severe hypertensive simulants get a 1 month followup and two drugs
         self.simulation.population.loc[severe_hypertension.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5*6) # 6 months
-        self.simulation.population.loc[severe_hypertension.index, 'taking_blood_pressure_medication_a'] = True
-        self.simulation.population.loc[severe_hypertension.index, 'taking_blood_pressure_medication_b'] = True
+
+        self.simulation.population.loc[severe_hypertension.index, 'medication_count'] = np.maximum(severe_hypertension['medication_count'] + 2, len(MEDICATIONS))
 
     def followup_blood_pressure_test(self, event):
-        self.cost_by_year[self.simulation.current_time.year] += len(event.affected_population) * 3.0
+        self.cost_by_year[self.simulation.current_time.year] += len(event.affected_population) * 9.73
 
         normotensive, hypertensive, severe_hypertension = _hypertensive_categories(event.affected_population)
 
-        nonmedicated_normotensive = normotensive.loc[(normotensive.taking_blood_pressure_medication_a == False) & (normotensive.taking_blood_pressure_medication_b == False)]
-        medicated_normotensive = normotensive.loc[(normotensive.taking_blood_pressure_medication_a == False) | (normotensive.taking_blood_pressure_medication_b == False)]
+        nonmedicated_normotensive = normotensive.loc[normotensive.medication_count == 0]
+        medicated_normotensive = normotensive.loc[normotensive.medication_count > 0]
 
         # Unmedicated normotensive simulants get a 60 month followup
         self.simulation.population.loc[nonmedicated_normotensive.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5*60) # 60 months
 
-        # Medicated normotensive simulants drop their drugs and get an 11 month followup
+        # Medicated normotensive simulants get an 11 month followup
         self.simulation.population.loc[medicated_normotensive.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5*11) # 11 months
-        self.simulation.population.loc[medicated_normotensive.index, 'taking_blood_pressure_medication_a'] = False
-        self.simulation.population.loc[medicated_normotensive.index, 'taking_blood_pressure_medication_b'] = False
 
         # Hypertensive simulants get a 6 month followup and go on one drug
-        # TODO: what if they are already taking drugs?
         self.simulation.population.loc[hypertensive.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5*6) # 6 months
-        self.simulation.population.loc[hypertensive.index, 'taking_blood_pressure_medication_a'] = True
-
-        # Severe hypertensive simulants get the same treatment as during a non-followup test
-        # TODO: is this right?
+        self.simulation.population.loc[hypertensive.index, 'medication_count'] = np.maximum(hypertensive['medication_count'] + 1, len(MEDICATIONS))
         self.simulation.population.loc[severe_hypertension.index, 'healthcare_followup_date'] = self.simulation.current_time + timedelta(days= 30.5*6) # 6 months
-        self.simulation.population.loc[severe_hypertension.index, 'taking_blood_pressure_medication_a'] = True
-        self.simulation.population.loc[severe_hypertension.index, 'taking_blood_pressure_medication_b'] = True
+        self.simulation.population.loc[severe_hypertension.index, 'medication_count'] = np.maximum(severe_hypertension.medication_count + 1, len(MEDICATIONS))
+
 
     @only_living
     def track_monthly_cost(self, event):
-        #TODO: realistic costs
-        for medication in ['medication_a', 'medication_b']:
-            medication_cost = self.simulation.config.getfloat('opportunistic_screening', medication + '_cost')
-            medication_cost *= self.simulation.config.getfloat('opportunistic_screening', 'adherence')
-            self.cost_by_year[self.simulation.current_time.year] += (event.affected_population['taking_blood_pressure_'+medication] == True).sum() * medication_cost*self.simulation.last_time_step.days
+        for medication_number in range(len(MEDICATIONS)):
+            user_count = sum(event.affected_population.medication_count > medication_number)
+            self.cost_by_year[self.simulation.current_time.year] += user_count * MEDICATIONS[medication_number]['daily_cost'] * self.simulation.last_time_step.days
 
     @only_living
     def adjust_blood_pressure(self, event):
         # TODO: Real drug effects + adherance rates
-        for medication in ['medication_a', 'medication_b']:
-            medication_effect = self.simulation.config.getfloat('opportunistic_screening', medication + '_effectiveness')
-            medication_effect *= self.simulation.config.getfloat('opportunistic_screening', 'adherence')
-            affected_population = event.affected_population.loc[event.affected_population['taking_blood_pressure_'+medication] == True]
-            self.simulation.population.loc[event.affected_population.index, 'systolic_blood_pressure'] -= medication_effect
+        for medication_number in range(len(MEDICATIONS)):
+            medication_efficacy = MEDICATIONS[medication_number]['efficacy'] * self.simulation.config.getfloat('opportunistic_screening', 'adherence')
+            affected_population = event.affected_population[event.affected_population.medication_count > medication_number]
+            self.simulation.population.loc[affected_population.index, 'systolic_blood_pressure'] -= medication_efficacy
 
 
 def confidence(seq):
