381fca1e384cba2787f5e7c483985ed865f8ce17
Alec Deason <alecwd@uw.edu>
2016-06-10 11:59:01 -0700

    some minor reorganization

--- ceam/engine.py
+++ ceam/engine.py
@@ -13,149 +13,9 @@ import pandas as pd
 import numpy as np
 np.seterr(all='raise')
 
-from ceam.util import from_yearly_rate, filter_for_rate
+from ceam.util import from_yearly, filter_for_rate
 from ceam.events import EventHandler, PopulationEvent, only_living
 from ceam.modules import ModuleRegistry
-
-
-class Simulation(ModuleRegistry):
-    def __init__(self, base_module_class=None):
-        ModuleRegistry.__init__(self, base_module_class if base_module_class is not None else BaseSimulationModule)
-
-        self.reference_data = {}
-        self.current_time = None
-        self.yll_by_year = defaultdict(float)
-        self.yld_by_year = defaultdict(float)
-        self.deaths_by_year_and_cause = defaultdict(lambda: defaultdict(int))
-        self.yll_by_year_and_cause = defaultdict(lambda: defaultdict(float))
-        self.new_cases_per_year = defaultdict(lambda: defaultdict(int))
-        self.population = pd.DataFrame()
-        self.lookup_table = pd.DataFrame()
-        self.config = ConfigParser()
-
-        config_path = os.path.abspath(os.path.dirname(__file__))
-        self.config.read([os.path.join(config_path, 'config.cfg'), os.path.join(config_path, 'local.cfg'), os.path.expanduser('~/ceam.cfg')])
-
-    def load_data(self, path_prefix=None):
-        if path_prefix is None:
-            path_prefix = self.config.get('general', 'reference_data_directory')
-
-        for module in self._ordered_modules:
-            module.load_data(path_prefix)
-        lookup_table = pd.DataFrame()
-
-        #TODO: This is ugly. There must be a better way
-        def column_prefixer(column, prefix):
-            if column not in ['age', 'year', 'sex']:
-                return prefix + '_' + column
-            return column
-
-        for module in self._ordered_modules:
-            if not module.lookup_table.empty:
-                prefixed_table = module.lookup_table.rename(columns=lambda c: column_prefixer(c, module.lookup_column_prefix))
-                assert prefixed_table.duplicated(['age','sex','year']).sum() == 0, "%s has a lookup table with duplicate rows"%(module.module_id())
-
-                if lookup_table.empty:
-                    lookup_table = prefixed_table
-                else:
-                    lookup_table = lookup_table.merge(prefixed_table, on=['age', 'sex', 'year'], how='inner')
-        lookup_table['lookup_id'] = range(0, len(lookup_table))
-        self.lookup_table = lookup_table
-
-    def load_population(self, path_prefix=None):
-        if path_prefix is None:
-            path_prefix = self.config.get('general', 'population_data_directory')
-
-        #NOTE: This will always be BaseSimulationModule which loads the core population definition and thus can discover what the population size is
-        module = self._ordered_modules[0]
-        module.load_population_columns(path_prefix, 0)
-        population_size = len(module.population_columns)
-
-        for module in self._ordered_modules[1:]:
-            module.load_population_columns(path_prefix, population_size)
-            assert module.population_columns.empty or len(module.population_columns) == population_size, 'Culpret: %s'%module
-        self.reset_population()
-
-    def reset_population(self):
-        population = pd.DataFrame()
-        for module in self._ordered_modules:
-            population = population.join(module.population_columns, how='outer')
-        self.population = population.join(pd.DataFrame(0, index=np.arange(len(population)), columns=['year']))
-
-    def index_population(self):
-        if not self.lookup_table.empty:
-            if 'lookup_id' in self.population:
-                self.population.drop('lookup_id', 1, inplace=True)
-            population = self.population.merge(self.lookup_table[['year','age','sex','lookup_id']], on=['year','age','sex'])
-            assert len(population) == len(self.population), "One of the lookup tables is missing rows or has duplicate rows"
-            self.population = population
-
-    def incidence_mediation_factor(self, label):
-        factor = 1
-        for module in self._ordered_modules:
-            factor *= 1 - module.incidence_mediation_factors.get(label, 0)
-        return 1 - factor
-
-
-    def emit_event(self, event):
-        for module in self._ordered_modules:
-            module.emit_event(event)
-
-    def mortality_rates(self, population):
-        rates = pd.Series(0, index=population.index)
-        for module in self._ordered_modules:
-            new_rates = module.mortality_rates(population, rates)
-            assert len(new_rates) == len(rates), "%s is corrupting mortality rates"%module
-            rates = new_rates
-        return from_yearly_rate(rates, self.last_time_step)
-
-    def incidence_rates(self, population, label):
-        rates = pd.Series(0, index=population.index)
-        for module in self._ordered_modules:
-            new_rates = module.incidence_rates(population, rates, label)
-            assert len(new_rates) == len(rates), "%s is corrupting incidence rates"%module
-            rates = new_rates
-        return from_yearly_rate(rates, self.last_time_step)
-
-    def disability_weight(self):
-        weights = 1
-        pop = self.population.loc[self.population.alive == True]
-        for module in self._ordered_modules:
-            weights *= 1 - module.disability_weight(pop)
-        total_weight = 1 - weights
-        return total_weight
-
-    def _verify_tables(self, start_time, end_time):
-        # Check that all the data necessary to run the requested date range is available
-        expected_index = set((age, sex, year) for age in range(1, 104) for sex in [1,2] for year in range(start_time.year, end_time.year))
-        for module in self._ordered_modules:
-            if not module.lookup_table.empty:
-                index = set(tuple(row) for row in module.lookup_table[['age','sex','year']].values.tolist())
-                assert len(expected_index.difference(index)) == 0, "%s has a lookup table that doesn't meet the minimal index requirements"%((module.module_id(),))
-
-    def _step(self, time_step):
-        self.last_time_step = time_step
-        self.population['year'] = self.current_time.year
-        self.population.loc[self.population.alive == True, 'fractional_age'] += time_step.days/365.0
-        self.population['age'] = self.population.fractional_age.astype(int)
-        self.index_population()
-        self.emit_event(PopulationEvent('time_step__continuous', self.population))
-        self.emit_event(PopulationEvent('time_step', self.population))
-        self.current_time += time_step
-
-    def run(self, start_time, end_time, time_step):
-        self._verify_tables(start_time, end_time)
-        self.reset_population()
-
-        self.current_time = start_time
-        while self.current_time <= end_time:
-            self._step(time_step)
-
-    def reset(self):
-        for module in self._ordered_modules:
-            module.reset()
-        self.reset_population()
-        self.current_time = None
 
 
 class SimulationModule(EventHandler):
@@ -237,6 +97,147 @@ class BaseSimulationModule(SimulationModule):
         if not affected_population.empty:
             self.simulation.population.loc[affected_population.index, 'alive'] = False
             self.simulation.emit_event(PopulationEvent('deaths', affected_population))
+
+class Simulation(ModuleRegistry):
+    def __init__(self, base_module_class=BaseSimulationModule):
+        ModuleRegistry.__init__(self, base_module_class)
+
+        self.reference_data = {}
+        self.current_time = None
+        self.yll_by_year = defaultdict(float)
+        self.yld_by_year = defaultdict(float)
+        self.deaths_by_year_and_cause = defaultdict(lambda: defaultdict(int))
+        self.yll_by_year_and_cause = defaultdict(lambda: defaultdict(float))
+        self.new_cases_per_year = defaultdict(lambda: defaultdict(int))
+        self.population = pd.DataFrame()
+        self.lookup_table = pd.DataFrame()
+        self.config = ConfigParser()
+
+        config_path = os.path.abspath(os.path.dirname(__file__))
+        self.config.read([os.path.join(config_path, 'config.cfg'), os.path.join(config_path, 'local.cfg'), os.path.expanduser('~/ceam.cfg')])
+
+    def load_data(self, path_prefix=None):
+        if path_prefix is None:
+            path_prefix = self.config.get('general', 'reference_data_directory')
+
+        for module in self._ordered_modules:
+            module.load_data(path_prefix)
+        lookup_table = pd.DataFrame()
+
+        #TODO: This is ugly. There must be a better way
+        def column_prefixer(column, prefix):
+            if column not in ['age', 'year', 'sex']:
+                return prefix + '_' + column
+            return column
+
+        for module in self._ordered_modules:
+            if not module.lookup_table.empty:
+                prefixed_table = module.lookup_table.rename(columns=lambda c: column_prefixer(c, module.lookup_column_prefix))
+                assert prefixed_table.duplicated(['age','sex','year']).sum() == 0, "%s has a lookup table with duplicate rows"%(module.module_id())
+
+                if lookup_table.empty:
+                    lookup_table = prefixed_table
+                else:
+                    lookup_table = lookup_table.merge(prefixed_table, on=['age', 'sex', 'year'], how='inner')
+        lookup_table['lookup_id'] = range(0, len(lookup_table))
+        self.lookup_table = lookup_table
+
+    def load_population(self, path_prefix=None):
+        if path_prefix is None:
+            path_prefix = self.config.get('general', 'population_data_directory')
+
+        #NOTE: This will always be BaseSimulationModule which loads the core population definition and thus can discover what the population size is
+        module = self._ordered_modules[0]
+        module.load_population_columns(path_prefix, 0)
+        population_size = len(module.population_columns)
+
+        for module in self._ordered_modules[1:]:
+            module.load_population_columns(path_prefix, population_size)
+            assert module.population_columns.empty or len(module.population_columns) == population_size, 'Culpret: %s'%module
+        self.reset_population()
+
+    def reset_population(self):
+        population = pd.DataFrame()
+        for module in self._ordered_modules:
+            population = population.join(module.population_columns, how='outer')
+        self.population = population.join(pd.DataFrame(0, index=np.arange(len(population)), columns=['year']))
+
+    def index_population(self):
+        if not self.lookup_table.empty:
+            if 'lookup_id' in self.population:
+                self.population.drop('lookup_id', 1, inplace=True)
+            population = self.population.merge(self.lookup_table[['year','age','sex','lookup_id']], on=['year','age','sex'])
+            assert len(population) == len(self.population), "One of the lookup tables is missing rows or has duplicate rows"
+            self.population = population
+
+    def incidence_mediation_factor(self, label):
+        factor = 1
+        for module in self._ordered_modules:
+            factor *= 1 - module.incidence_mediation_factors.get(label, 1)
+        return 1 - factor
+
+
+    def emit_event(self, event):
+        for module in self._ordered_modules:
+            module.emit_event(event)
+
+    def mortality_rates(self, population):
+        rates = pd.Series(0, index=population.index)
+        for module in self._ordered_modules:
+            new_rates = module.mortality_rates(population, rates)
+            assert len(new_rates) == len(rates), "%s is corrupting mortality rates"%module
+            rates = new_rates
+        return from_yearly(rates, self.last_time_step)
+
+    def incidence_rates(self, population, label):
+        rates = pd.Series(0, index=population.index)
+        for module in self._ordered_modules:
+            new_rates = module.incidence_rates(population, rates, label)
+            assert len(new_rates) == len(rates), "%s is corrupting incidence rates"%module
+            rates = new_rates
+        return from_yearly(rates, self.last_time_step)
+
+    def disability_weight(self):
+        weights = 1
+        pop = self.population.loc[self.population.alive == True]
+        for module in self._ordered_modules:
+            weights *= 1 - module.disability_weight(pop)
+        total_weight = 1 - weights
+        return total_weight
+
+    def _verify_tables(self, start_time, end_time):
+        # Check that all the data necessary to run the requested date range is available
+        expected_index = set((age, sex, year) for age in range(1, 104) for sex in [1,2] for year in range(start_time.year, end_time.year))
+        for module in self._ordered_modules:
+            if not module.lookup_table.empty:
+                index = set(tuple(row) for row in module.lookup_table[['age','sex','year']].values.tolist())
+                assert len(expected_index.difference(index)) == 0, "%s has a lookup table that doesn't meet the minimal index requirements"%((module.module_id(),))
+
+    def _step(self, time_step):
+        self.last_time_step = time_step
+        self.population['year'] = self.current_time.year
+        self.population.loc[self.population.alive == True, 'fractional_age'] += time_step.days/365.0
+        self.population['age'] = self.population.fractional_age.astype(int)
+        self.index_population()
+        self.emit_event(PopulationEvent('time_step__continuous', self.population))
+        self.emit_event(PopulationEvent('time_step', self.population))
+        self.current_time += time_step
+
+    def run(self, start_time, end_time, time_step):
+        self._verify_tables(start_time, end_time)
+        self.reset_population()
+
+        self.current_time = start_time
+        while self.current_time <= end_time:
+            self._step(time_step)
+
+    def reset(self):
+        for module in self._ordered_modules:
+            module.reset()
+        self.reset_population()
+        self.current_time = None
+
+
 
 
 # End.
--- ceam/modules/healthcare_access.py
+++ ceam/modules/healthcare_access.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from ceam.engine import SimulationModule
 from ceam.events import PopulationEvent, only_living
-from ceam.util import filter_for_rate, filter_for_probability, from_yearly_rate
+from ceam.util import filter_for_rate, filter_for_probability, from_yearly
 
 
 class HealthcareAccessModule(SimulationModule):
@@ -35,7 +35,7 @@ class HealthcareAccessModule(SimulationModule):
 
     @only_living
     def general_access(self, event):
-        affected_population = filter_for_rate(event.affected_population, from_yearly_rate(self.lookup_columns(event.affected_population, ['rate'])['rate'], self.simulation.last_time_step))
+        affected_population = filter_for_rate(event.affected_population, from_yearly(self.lookup_columns(event.affected_population, ['rate'])['rate'], self.simulation.last_time_step))
         self.simulation.population.loc[affected_population.index, 'healthcare_last_visit_date'] = self.simulation.current_time
         self.simulation.emit_event(PopulationEvent('general_healthcare_access', affected_population))
         self.cost_by_year[self.simulation.current_time.year] += len(affected_population) * 7.30
