8e2cf23186dd772d193688728595fffb6b26c810
Alec Deason <alecwd@uw.edu>
2016-06-13 12:29:05 -0700

    Allow loading alternate data files as the initial data for a condition (mostly for testing)

--- ceam/modules/chronic_condition.py
+++ ceam/modules/chronic_condition.py
@@ -9,17 +9,32 @@ from ceam.util import filter_for_rate
 from ceam.events import only_living
 
 class ChronicConditionModule(SimulationModule):
-    '''
+    """
     A generic module that can handle any simple condition
-    '''
+    """
     # TODO: expand this to handle acute vs chronic states, hopefully that can be generic too
 
-    def __init__(self, condition, mortality_table_name, incidence_table_name, disability_weight):
+    def __init__(self, condition, mortality_table_name, incidence_table_name, disability_weight, initial_column_table_name=None):
+        """
+        Parameters
+        ----------
+        condition : str
+            Name of the chronic condition. Used for column names and for incidence/mortality rate queries
+        mortality_table_name : str
+            Name of the table to load mortality rates from. Will search in the standard data directory.
+        incidence_table_name : str
+            Name of the table to load incidence rates from. Will search in the standard data directory.
+        disability_weight : float
+            Disability weight of this condition
+        initial_column_table_name : str
+            Name of the column table that contains the inital state of this condition. If None then `condition`.csv will be used. Will search in the standard population columns directory.
+        """
         SimulationModule.__init__(self)
         self.condition = condition
         self.mortality_table_name = mortality_table_name
         self.incidence_table_name = incidence_table_name
         self._disability_weight = disability_weight
+        self._initial_column_table_name = initial_column_table_name
 
     def setup(self):
         self.register_event_listener(self.incidence_handler, 'time_step')
@@ -28,9 +43,18 @@ class ChronicConditionModule(SimulationModule):
         return (self.__class__, self.condition)
 
     def load_population_columns(self, path_prefix, population_size):
-        if os.path.exists(os.path.join(path_prefix, self.condition+'.csv')):
-            # If an initial population column exists with the name of this condition, load it
-            self.population_columns = pd.read_csv(os.path.join(path_prefix, self.condition+'.csv'))
+        initial_column = None
+        if self._initial_column_table_name:
+            initial_column = pd.read_csv(os.path.join(path_prefix, self._initial_column_table_name))
+        else:
+            table_name = self.condition + '.csv'
+            if os.path.exists(os.path.join(path_prefix, table_name)):
+                # If an initial population column exists with the name of this condition, load it
+                initial_column = pd.read_csv(os.path.join(path_prefix, table_name))
+
+        if initial_column is not None:
+            self.population_columns = initial_column
+            self.population_columns.columns = [self.condition]
             self.population_columns[self.condition] = self.population_columns[self.condition].astype(bool)
         else:
             # If there's no initial data for this condition, start everyone healthy
--- ceam_tests/test_modules/test_chronic_condition.py
+++ ceam_tests/test_modules/test_chronic_condition.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pytest
 
 from ceam.modules.chronic_condition import ChronicConditionModule
@@ -27,3 +29,27 @@ def test_disability_weight():
     illnesses = [1,2,3,4,5]
     simulation.population.loc[illnesses, ['no_initial_disease']] = True
     assert round(simulation.disability_weight().sum() - 0.01*len(illnesses), 5) == 0
+
+def test_initial_column_data():
+    data_path = os.path.join(str(pytest.config.rootdir), 'ceam_tests', 'test_data', 'population_columns')
+
+    module = ChronicConditionModule('high_initial_disease', 'mortality_0.7.csv', 'incidence_0.0.csv', 0.01)
+    module.load_population_columns(data_path, 1000)
+
+    # There is a initial column file with this name. Make sure we got data
+    assert module.population_columns.high_initial_disease.sum() > 0
+
+    module = ChronicConditionModule('some_crazy_disease', 'mortality_0.7.csv', 'incidence_0.0.csv', 0.01)
+    module.load_population_columns(data_path, 1000)
+    # There is no initial column file with this name. Make sure we didn't get data
+    assert module.population_columns.some_crazy_disease.sum() == 0
+
+    module = ChronicConditionModule('some_crazy_disease', 'mortality_0.7.csv', 'incidence_0.0.csv', 0.01, initial_column_table_name='high_initial_disease.csv')
+    module.load_population_columns(data_path, 1000)
+    # Even though there's no initial data for this condition we've specified an alternate table to load so we should use the data from that
+    assert module.population_columns.some_crazy_disease.sum() > 0
+
+    module = ChronicConditionModule('some_crazy_disease', 'mortality_0.7.csv', 'incidence_0.0.csv', 0.01, initial_column_table_name='some_crazy_disease.csv')
+    with pytest.raises(OSError):
+        # We're explicitly specifying a table to load but it doesn't exist. Fail fast
+        module.load_population_columns(data_path, 1000)
