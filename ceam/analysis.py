61c76651d3e0c5b1b869a847ad35da304bc954e9
Alec Deason <alecwd@uw.edu>
2016-06-24 10:46:40 -0700

    calculate qualies

--- ceam/analysis.py
+++ ceam/analysis.py
@@ -8,8 +8,8 @@ import numpy as np
 def confidence(seq):
     mean = np.mean(seq)
     std = np.std(seq)
-    runs = len(seq)
-    interval = (1.96*std)/np.sqrt(runs)
+    n = len(seq)
+    interval = (1.96*std)/np.sqrt(n)
     return int(mean), int(mean-interval), int(mean+interval)
 
 def difference_with_confidence(a, b):
@@ -24,12 +24,27 @@ def analyze_results(results):
     i_dalys = intervention.ylds + intervention.ylls
     ni_dalys = non_intervention.ylds + non_intervention.ylls
 
+    cpd = sorted(intervention.intervention_cost.values/(ni_dalys.values-i_dalys.values))
+    cpd_lower = int(cpd[int(len(cpd)*0.025)])
+    cpd_upper = int(cpd[int(len(cpd)*0.975)])
+
+    if 'qualys' in non_intervention:
+        cpq = sorted(intervention.intervention_cost.values/(intervention.qualys.values - non_intervention.qualys.values))
+        cpq_lower = int(cpq[int(len(cpq)*0.025)])
+        cpq_upper = int(cpq[int(len(cpq)*0.975)])
+
+
+
     print('Total runs', len(intervention))
     print('Mean duration', int(results.duration.mean()))
     print('DALYs (intervention)', confidence(i_dalys), 'DALYs (non-intervention)', confidence(ni_dalys))
     print('DALYs averted', difference_with_confidence(ni_dalys,i_dalys))
+    if 'qualys' in non_intervention:
+        print('QUALYs gained', difference_with_confidence(intervention.qualys, non_intervention.qualys))
     print('Total Intervention Cost', confidence(intervention.intervention_cost))
-    print('Cost per DALY', confidence(intervention.intervention_cost.values/(ni_dalys.values-i_dalys.values)))
+    print('Cost per DALY averted', (int(np.mean(cpd)), cpd_lower, cpd_upper))
+    if 'qualys' in non_intervention:
+        print('Cost per QUALY gained', (int(np.mean(cpq)), cpq_lower, cpq_upper))
     print('IHD Count (intervention)',confidence(intervention.ihd_count), 'IHD Count (non-intervention)', confidence(non_intervention.ihd_count))
     print('Stroke Count (intervention)',confidence(intervention.hemorrhagic_stroke_count), 'Stroke Count (non-intervention)', confidence(non_intervention.hemorrhagic_stroke_count))
 
