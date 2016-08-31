c8aa8b27724b0a8947fbb16f0db355e738e5440e
Alec Deason <alecwd@uw.edu>
2016-08-31 11:12:54 -0700

    try tweaking setup.py to actually install non-python files

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, find_packages
 setup(name='ceam',
         version='0.1',
         packages=find_packages(),
+        include_package_data=True,
         install_requires=[
             'pandas',
             'numpy',
