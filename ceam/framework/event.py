6b3a9520f2c94e818986ee7a9b547a1d83e46b92
Alec Deason <alecwd@uw.edu>
2016-09-02 16:02:03 -0700

    More docs

--- ceam/framework/event.py
+++ ceam/framework/event.py
@@ -6,24 +6,34 @@ from collections import defaultdict
 from .util import marker_factory, resource_injector
 
 listens_for = marker_factory('event_system__listens_for', with_priority=True)
+listens_for.__doc__ = """Mark a function as a listener for the named event so that
+the simulation will call it when that event occurs.
+"""
+
 emits = resource_injector('event_system__emits')
+emits.__doc__ = """Mark a function as an emitter for the named event. An event emitter function
+which can be called to emit the named event will be injected into the functions
+arguments whenever it is called.
+"""
 
 class Event:
-    """The parent class of all events
+    """An Event object represents the context of an event. It is possible to subclass Event
+    to add information for more specialized cases like ceam.framework.population.PopulationEvent
 
     Attributes
     ----------
     time  : datetime.datetime
-            The simulation time at which this event was emited.
+            The simulation time at which this event was emitted.
     index : pandas.Index
             An index into the population table containing all simulants effected by this event.
     """
+
     def __init__(self, time, index):
         self.time = time
         self.index = index
 
     def split(self, new_index):
-        """Create a new event with all the attributes of this event but a new index.
+        """Create a new event which is a copy of this one but with a new index.
         """
         return Event(self.time, new_index)
 
@@ -38,13 +48,34 @@ class _EventChannel:
 
 
 class EventManager:
-    """Manages the configuration of the event system
+    """The configuration for the event system.
+
+    Notes
+    -----
+    Client code should never need to interact with this class
+    except through the decorators in this module and the emitter
+    function exposed on the builder during the setup phase.
     """
+
     def __init__(self):
         self.__event_types = defaultdict(_EventChannel)
 
-    def get_emitter(self, label):
-        return self.__event_types[label].emit
+    def get_emitter(self, name):
+        """Get an emitter function for the named event
+
+        Parameters
+        ----------
+        name : str
+               The name of the event
+
+        Returns
+        -------
+        callable
+            A function that accepts an Event object and distributes
+            it to all listeners for this event.
+        """
+
+        return self.__event_types[name].emit
 
     def _emitter_injector(self, func, args, kwargs, label):
         return list(args) + [self.__event_types[label]], kwargs
@@ -66,8 +97,16 @@ class EventManager:
                 self.get_emitter(*args, **kwargs)
 
     def list_events(self):
-        """Return a list of all event labels known to the event system
+        """List all event names known to the event system
 
+        Returns
+        -------
+        list
+            A list of all known event names
+
+        Notes
+        -----
         This value can change after setup if components dynamically create new event labels.
         """
+
         return list(self.__event_types.keys())
--- ceam/framework/population.py
+++ ceam/framework/population.py
@@ -1,36 +1,104 @@
+"""
+"""
 import pandas as pd
+
+from ceam import CEAMError
 
 from .util import resource_injector
 from .event import listens_for, Event
 
-population_view = resource_injector('population_system_population_view')
-uses_columns = population_view
+uses_columns = resource_injector('population_system_population_view')
+uses_columns.__doc__ = """Mark a function as a user of columns from the population table. If the
+function is also an event listener then the Event object which it receives will be transformed into a
+PopulationEvent. Otherwise the function will have a configured PopulationView injected into its arguments.
+
+Parameters
+----------
+columns : [str]
+          A list of column names which the function will need to read or write.
+query   : str
+          A filter in pandas query syntax which should be applied to the population before it made accessible
+          to this function. This effects both the ``population`` and the ``index`` attributes of PopulationEvents
+"""
+
+class PopulationError(CEAMError):
+    pass
 
 class PopulationView:
+    """A PopulationView provides access to the simulations population table. It can be used to both read and write
+    the state of the population. A PopulationView can only read and write columns for which it is configured. Attempts
+    to write to non-existent columns are ignored except during resolution of the ``generate_population`` event when new
+    columns are allowed to be created.
+
+    Attributes
+    ----------
+    columns : [str] (read only)
+              The columns for which this view is configured
+    query   : str (read only)
+              The query which will be used to filter the population table for this view. This query may reference columns
+              not in the view's columns.
+
+    Notes
+    -----
+    PopulationViews can only be created by the simulation itself. Client code receives them via functions decorated
+    by ``uses_columns`` or the builder's ``population_view`` method during setup.
+    """
+
     def __init__(self, manager, columns, query):
         self.manager = manager
-        self.columns = columns
-        self.query = query
+        self._columns = columns
+        self._query = query
+
+    @property
+    def columns(self):
+        return list(self._columns)
+
+    @property
+    def query(self):
+        return self._query
 
     def get(self, index):
+        """For the rows in ``index`` get the columns from the simulation's population which this view is configured.
+        The result may be further filtered by the view's query.
+
+        Parameters
+        ----------
+        index : pandas.Index
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
+
         pop = self.manager._population.ix[index]
-        if self.query:
-            pop = pop.query(self.query)
-        return pop[self.columns].copy()
+        if self._query:
+            pop = pop.query(self._query)
+        return pop[self._columns].copy()
 
     def update(self, pop):
+        """Update the simulation's state to match ``pop``
+
+        Parameters
+        ----------
+        pop : pandas.DataFrame or pandas.Series
+              The data which should be copied into the simulation's state. If ``pop`` is a DataFrame only those columns
+              included in the view's columns will be used. If ``pop`` is a Series it must have a name that matches
+              one of the view's columns unless the view only has one column in which case the Series will be assumed to
+              refer to that regardless of its name.
+        """
+
         if not pop.empty:
             if isinstance(pop, pd.Series):
-                if pop.name in self.columns:
+                if pop.name in self._columns:
                     affected_columns = [pop.name]
-                elif len(self.columns) == 1:
-                    affected_columns = self.columns
+                elif len(self._columns) == 1:
+                    affected_columns = self._columns
                 else:
-                    raise ValueError('Cannot update with a Series unless the series name equals a column name or there is only a single column in the view')
+                    raise PopulationError('Cannot update with a Series unless the series name equals a column name or there is only a single column in the view')
             else:
                 affected_columns = set(pop.columns)
 
-            affected_columns = set(affected_columns).intersection(self.columns)
+            affected_columns = set(affected_columns).intersection(self._columns)
             if self.manager.initialized:
                 affected_columns = set(affected_columns).intersection(self.manager._population.columns)
 
@@ -50,6 +118,20 @@ class PopulationView:
                 self.manager._population[c] = v
 
 class PopulationEvent(Event):
+    """A standard Event with additional population data. This is the type of event that functions decorated with both
+    ``listens_for`` and ``uses_columns`` will receive.
+
+    Attributes
+    ----------
+    population      : pandas.DataFrame
+                      A copy of the subset of the simulation's population table selected by applying the underlying Event's
+                      index to the PopulationView which created this event
+    population_view : PopulationView
+                      The PopulationView which created this event
+    index           : pandas.Index
+                      The index of the underlying Event filtered by the PopulationView's query, if any.
+    """
+
     def __init__(self, time, index, population, population_view):
         super(PopulationEvent, self).__init__(time, index)
         self.population = population
@@ -65,11 +147,30 @@ class PopulationEvent(Event):
 
 
 class PopulationManager:
+    """The configuration for the population management system.
+
+        Notes
+        -----
+        Client code should never need to interact with this class
+        except through the ``population_view`` function on the builder
+        during setup.
+    """
+
     def __init__(self):
         self._population = pd.DataFrame()
         self.initialized = False
 
     def get_view(self, columns, query=None):
+        """Return a configured PopulationView
+
+        Notes
+        -----
+        Client code should only need this (and only through the version exposed as
+        ``population_view`` on the builder during setup) if it uses dynamically
+        generated column names that aren't known at definition time. Otherwise
+        components should use ``uses_columns``.
+        """
+
         return PopulationView(self, columns, query)
 
     def _injector(self, func, args, kwargs, columns, query=None):
@@ -93,7 +194,7 @@ class PopulationManager:
         return args, kwargs
 
     def setup_components(self, components):
-        population_view.set_injector(self._injector)
+        uses_columns.set_injector(self._injector)
 
     @property
     def population(self):
