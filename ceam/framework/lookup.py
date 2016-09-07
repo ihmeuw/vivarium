05bc6fb4caefcd76c8d40cc4e5a08e3f15048555
Alec Deason <alecwd@uw.edu>
2016-09-07 10:19:55 -0700

    Docstrings for lookup

--- ceam/framework/lookup.py
+++ ceam/framework/lookup.py
@@ -8,54 +8,81 @@ from .event import listens_for
 from .population import uses_columns
 
 class TableView:
-    def __init__(self, manager, key, index, columns):
-        self.manager = manager
-        self.index = index
-        self.column_map = {key + '_' + c:c for c in columns}
+    """A callable that looks up columns in the merged lookup table for simulant in an index
+
+    Parameters
+    ----------
+    index : pandas.Index
+            Retrieve the data contained in this table corresponding to each simulant in the index.
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+    def __init__(self, manager, prefix, table_group, columns):
+        self._manager = manager
+        self._table_group = table_group
+        self._column_map = {prefix + '_' + c:c for c in columns}
 
     def __call__(self, pop):
         if isinstance(pop, pd.Index):
             index = pop
         else:
             index = pop.index
-        result = self.manager._current_table[self.index].ix[index, list(self.column_map.keys())]
+        result = self._manager._current_table[self._table_group].ix[index, list(self._column_map.keys())]
 
         if len(result.columns) > 1:
-            return result.rename(columns=self.column_map)
+            # Column names are mangled in the underlying table to prevent collisions
+            # so they are unmangled here before returning them to the user.
+            return result.rename(columns=self._column_map)
         else:
             return result[result.columns[0]]
 
 class MergedTableManager:
+    """Container for the merged lookup tables
+
+    Notes
+    -----
+    Client code should never access this class directly. Use ``lookup`` on the builder during setup
+    to get references to TableView objects.
+    """
+
     def __init__(self):
         self._base_table = defaultdict(lambda: None)
         self._current_table = defaultdict(lambda: None)
         self.last_year = None
 
-    def uses(self, table_name):
-        def wrapper(consumer):
-            def inner(*args, **kwargs):
-                def getter(population, column):
-                    return self._current_table.ix[population.simulant_id, table_name + '_' +column]
-                args = list(args) + [getter]
-                return consumer(*args, **kwargs)
-            return inner
-        return wrapper
+    def build_table(self, data, key_columns=('age', 'sex', 'year')):
+        """Construct a TableView from a ``pandas.DataFrame``. The contents of ``data`` will be merged
+        with with other reference data for fast access later.
 
-    def build_table(self, data, index=('age', 'sex', 'year')):
-        index = tuple(index)
+        Parameters
+        ----------
+        data        : pandas.DataFrame
+                      The source data which will be accessible through the resulting TableView.
+        key_colmuns : [str]
+                      The columns which make up the key for this data. These will be used to reindex the
+                      data relative to the population table so that it can be accessed by simulant id.
+
+        Returns
+        -------
+        TableView
+        """
+
+        key_columns = tuple(key_columns)
         table = data
-        key = str(uuid.uuid4())
+        prefix = str(uuid.uuid4())
         columns = table.columns
-        column_map = {c:key + '_' + c for c in table.columns if c not in index}
+        column_map = {c:prefix + '_' + c for c in table.columns if c not in key_columns}
         table = table.rename(columns=column_map)
         if 'sex' in table:
             table['sex'] = table.sex.astype('category')
-        if self._base_table[index] is None:
-            self._base_table[index] = table
+        if self._base_table[key_columns] is None:
+            self._base_table[key_columns] = table
         else:
-            self._base_table[index] = self._base_table[index].merge(table, on=index, how='inner')
+            self._base_table[key_columns] = self._base_table[key_columns].merge(table, on=key_columns, how='inner')
 
-        return TableView(self, key, index, [c for c in columns if c not in index])
+        return TableView(self, prefix, key_columns, [c for c in columns if c not in key_columns])
 
     @listens_for('time_step__prepare')
     @uses_columns(['age', 'sex'])
