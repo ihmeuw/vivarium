e3d5e216185bbb65489add1c2eadabbab0864321
Alec Deason <alecwd@uw.edu>
2016-09-09 09:56:20 -0700

    Give the simulate executable an extension so Windows knows what to do with it.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='ceam',
         version='0.1',
         packages=find_packages(),
         include_package_data=True,
-        scripts=['simulate'],
+        scripts=['simulate.py'],
         install_requires=[
             'pandas',
             'numpy',
