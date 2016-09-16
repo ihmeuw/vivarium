991825f26069a27fc48a0bf4c6ee60ebf2dacd30
Alec Deason <alecwd@uw.edu>
2016-09-16 11:37:27 -0700

    Rough treatment of reproductive age

--- ceam/components/fertility.py
+++ ceam/components/fertility.py
@@ -3,12 +3,15 @@ from datetime import timedelta
 import pandas as pd
 import numpy as np
 
+from scipy.stats import norm
+
 from ceam import config
 
 from ceam.framework.util import rate_to_probability, from_yearly
 
 from ceam.framework.state_machine2 import Transition, TransitionSet, record_event_time, active_after_delay, new_state_side_effect
 from ceam.framework.event import listens_for
+from ceam.framework.values import produces_value
 from ceam.framework.population import uses_columns, creates_simulants
 
 class Fertility:
@@ -30,6 +33,8 @@ class Fertility:
         self.transitions.table['pregnant'].append(birth)
         self.transitions.table['pregnant'].append(Transition.RESIDUAL)
 
+        self.conception_rate = builder.rate('fertility.conception_rate')
+
         return [self.transitions]
 
     @listens_for('time_step')
@@ -37,12 +42,20 @@ class Fertility:
     def step(self, event):
         self.transitions.transition(event.population.pregnancy)
 
+    @produces_value('fertility.conception_rate')
+    @uses_columns(['fertility', 'fractional_age'], 'pregnancy == "not_pregnant"')
+    def base_conception_rate(self, index, population_view):
+        pop = population_view.get(index)
+        reproductive_age_dist = norm.pdf((pop.fractional_age-25)/4)
+        age_adjusted_fertility = pop.fertility * reproductive_age_dist
+        return age_adjusted_fertility/0.35 # scale to make the total fertility rate roughly match Africa's
+
     @listens_for('initialize_simulants')
     @uses_columns(['sex', 'fertility'])
     def make_fertility_column(self, event):
         fertility = pd.Series(0.0, name='fertility', index=event.index)
         women = event.population.sex == 'Female'
-        fertility[women] = np.random.random(size=women.sum())*0.1
+        fertility[women] = np.random.random(size=women.sum())
         event.population_view.update(fertility)
 
     @listens_for('initialize_simulants')
@@ -50,12 +63,8 @@ class Fertility:
     def make_pregnancy_column(self, event):
         event.population_view.update(pd.Series('not_pregnant', index=event.index))
 
-    @uses_columns(['fertility'])
-    def conception_probability(self, index, population_view):
-        population = population_view.get(index)
-        time_step = config.getfloat('simulation_parameters', 'time_step')
-        time_step = timedelta(days=time_step)
-        return rate_to_probability(from_yearly(population.fertility, time_step))
+    def conception_probability(self, index):
+        return rate_to_probability(self.conception_rate(index))
 
     @creates_simulants
     def add_child(self, index, creator):
