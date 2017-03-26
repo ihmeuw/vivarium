dceaf534a464cf1b8011262489f0d025d8cda0b9
emumford <emumford@uw.edu>
2017-03-26 11:16:19 -0700

    Improved asthetics (PEP8) + comments

--- ceam_tests/framework/test_risks/test_risk_deletion_and_rr.py
+++ ceam_tests/framework/test_risks/test_risk_deletion_and_rr.py
@@ -22,45 +22,59 @@ def set_up_test_parameters(simulation, multiple_risks_test):
 
     Parameters
     ----------
-    simulation: object @alecwd: Is 'object' the correct way to classify 'simulation'?
+    simulation: object
         CEAM simulation object
-    
+
     multiple_risks_test: Bool
-        Set to true if doing a test of a simulation with multiple risk factors 
+        Set to true if testing a cause associated with multiple risk factors
+        False if testing a cause associated with only one risk factor
     """
-    # Set base incidence of rotavirus to 10 arbitrarily
-    simulation_incidence = simulation.values.get_rate('base_incidence_rate.diarrhea_due_to_rotaviral_entiritis')
+
+    # Set base incidence of diarrhea due to rotavirus to 10 arbitrarily
+    simulation_incidence = simulation.values.get_rate(
+        'base_incidence_rate.diarrhea_due_to_rotaviral_entiritis')
     incidence_rota = build_table(10, ['age', 'year', 'sex', 'rate'])
     simulation_incidence.source = simulation.tables.build_table(incidence_rota)
 
     # Set PAF arbitrarily to .75 for stunting
     stunting_paf = build_table(.75, ['age', 'year', 'sex', 'PAF'])
-    simulation_stunting_paf = simulation.values.get_value('paf_of_stunting_on_diarrhea_due_to_rotaviral_entiritis')
-    simulation_stunting_paf.source = simulation.tables.build_table(stunting_paf)
+    simulation_stunting_paf = simulation.values.get_value(
+        'paf_of_stunting_on_diarrhea_due_to_rotaviral_entiritis')
+    simulation_stunting_paf.source = simulation.tables.build_table(
+        stunting_paf)
 
-    # Set RR arbitrarily to 2 for severe stunting, 1 for all other categories
+    # Set relative risk arbitrarily to 2 for severe stunting, 1 for all other
+    #     categories
     stunting_rr = build_table(2, ['age', 'year', 'sex', 'cat1'])
     stunting_rr['cat2'] = 1
     stunting_rr['cat3'] = 1
     stunting_rr['cat4'] = 1
 
-    simulation_stunting_relative_risk = simulation.values.get_value('relative_risk_of_stunting_on_diarrhea_due_to_rotaviral_entiritis')    
-    simulation_stunting_relative_risk.source = simulation.tables.build_table(stunting_rr)
+    simulation_stunting_relative_risk = simulation.values.get_value(
+        'relative_risk_of_stunting_on_diarrhea_due_to_rotaviral_entiritis')
+    simulation_stunting_relative_risk.source = simulation.tables.build_table(
+        stunting_rr)
 
-    # If testing a simulation with 2 risk factors, set PAF and RR for unsafe_sanitation as well
+    # If testing a simulation with 2 risk factors, set PAF and RR for
+    #     unsafe_sanitation as well
     if multiple_risks_test:
         # Set PAF arbitrarily to .25 for unsafe sanitation
         unsafe_sanitation_paf = build_table(.25, ['age', 'year', 'sex', 'PAF'])
-        simulation_unsafe_sanitation_paf = simulation.values.get_value('paf_of_unsafe_sanitation_on_diarrhea_due_to_rotaviral_entiritis')
-        simulation_unsafe_sanitation_paf.source = simulation.tables.build_table(unsafe_sanitation_paf)
+        simulation_unsafe_sanitation_paf = simulation.values.get_value(
+            'paf_of_unsafe_sanitation_on_diarrhea_due_to_rotaviral_entiritis')
+        simulation_unsafe_sanitation_paf.source = simulation.tables.build_table(
+            unsafe_sanitation_paf)
 
-        # Set RR arbitrarily to 2 for severe unsafe_sanitation, 1 for all other categories
+        # Set RR arbitrarily to 2 for severe unsafe_sanitation, 1 for all other
+        #     categories
         unsafe_sanitation_rr = build_table(2, ['age', 'year', 'sex', 'cat1'])
         unsafe_sanitation_rr['cat2'] = 1
         unsafe_sanitation_rr['cat3'] = 1
 
-        simulation_unsafe_sanitation_relative_risk = simulation.values.get_value('relative_risk_of_unsafe_sanitation_on_diarrhea_due_to_rotaviral_entiritis')
-        simulation_unsafe_sanitation_relative_risk.source = simulation.tables.build_table(unsafe_sanitation_rr)
+        simulation_unsafe_sanitation_relative_risk = simulation.values.get_value(
+            'relative_risk_of_unsafe_sanitation_on_diarrhea_due_to_rotaviral_entiritis')
+        simulation_unsafe_sanitation_relative_risk.source = simulation.tables.build_table(
+            unsafe_sanitation_rr)
 
     return simulation
 
@@ -71,119 +85,169 @@ def set_up_exposures(simulation, multiple_risks_test, full_exposure):
     """
     Parameters
     ----------
-    simulation: object @alecwd: Is 'object' the correct way to classify 'simulation'?
+    simulation: object
         CEAM simulation object
 
     multiple_risks_test: Bool
         Set to true if doing a test of a simulation with multiple risk factors
 
     full_exposure: Bool
-        set to true to set exposure to the most severe category to 100%, false to set exposure to the least severe category to 100%
+        set to true to set exposure to the most severe category to 100%
+        set to false to set exposure to the least severe category to 100%
     """
+    # set exposure to category 4 (no stunting) to 100%
     if not full_exposure:
         stunting_exposure = build_table(0, ['age', 'year', 'sex', 'cat1'])
         stunting_exposure['cat2'] = 0
         stunting_exposure['cat3'] = 0
         stunting_exposure['cat4'] = 1
 
-        simulation_stunting_exposure = simulation.values.get_rate('stunting.exposure')
-        simulation_stunting_exposure.source = simulation.tables.build_table(stunting_exposure)
+        simulation_stunting_exposure = simulation.values.get_rate(
+            'stunting.exposure')
+        simulation_stunting_exposure.source = simulation.tables.build_table(
+            stunting_exposure)
 
+    # set exposure to category 1 (severe stunting) to 100%
     else:
         stunting_exposure = build_table(1, ['age', 'year', 'sex', 'cat1'])
         stunting_exposure['cat2'] = 0
         stunting_exposure['cat3'] = 0
         stunting_exposure['cat4'] = 0
 
-        simulation_stunting_exposure = simulation.values.get_rate('stunting.exposure')
-        simulation_stunting_exposure.source = simulation.tables.build_table(stunting_exposure)
+        simulation_stunting_exposure = simulation.values.get_rate(
+            'stunting.exposure')
+        simulation_stunting_exposure.source = simulation.tables.build_table(
+            stunting_exposure)
 
     if multiple_risks_test:
+        # set exposure to category 3 (connection to sewer, i.e. TMREL) to 100%
         if not full_exposure:
-            unsafe_sanitation_exposure = build_table(0, ['age', 'year', 'sex', 'cat1'])
+            unsafe_sanitation_exposure = build_table(0, ['age', 'year', 'sex',
+                                                         'cat1'])
             unsafe_sanitation_exposure['cat2'] = 0
             unsafe_sanitation_exposure['cat3'] = 1
 
-            simulation_unsafe_sanitation_exposure = simulation.values.get_rate('unsafe_sanitation.exposure')
-            simulation_unsafe_sanitation_exposure.source = simulation.tables.build_table(unsafe_sanitation_exposure)
+            simulation_unsafe_sanitation_exposure = simulation.values.get_rate(
+                'unsafe_sanitation.exposure')
+            simulation_unsafe_sanitation_exposure.source = simulation.tables.build_table(
+                unsafe_sanitation_exposure)
 
+        # set exposure to category 1 (unimproved, i.e. highest RR) to 100%
         else:
-            unsafe_sanitation_exposure = build_table(1, ['age', 'year', 'sex', 'cat1'])
+            unsafe_sanitation_exposure = build_table(1, ['age', 'year', 'sex',
+                                                         'cat1'])
             unsafe_sanitation_exposure['cat2'] = 0
             unsafe_sanitation_exposure['cat3'] = 0
 
-            simulation_unsafe_sanitation_exposure = simulation.values.get_rate('unsafe_sanitation.exposure')
-            simulation_unsafe_sanitation_exposure.source = simulation.tables.build_table(unsafe_sanitation_exposure)
-   
+            simulation_unsafe_sanitation_exposure = simulation.values.get_rate(
+                'unsafe_sanitation.exposure')
+            simulation_unsafe_sanitation_exposure.source = simulation.tables.build_table(
+                unsafe_sanitation_exposure)
+
     return simulation
 
 
-#######################################################################
-# Step 2. Conduct tests in a simulation that includes one risk factor #
-#######################################################################
+########################################################################
+# Step 2. Conduct tests for a cause associated with only 1 risk factor #
+########################################################################
 
-# FIXME: Expect one risk simulation tests to fail until the PAF pipeline is set up to return base incidence * (1 - PAF) for a one risk simulation
-
+# FIXME: Expect one risk simulation tests to fail until the PAF pipeline is set
+#     up to return base incidence * (1 - PAF) for a cause with one risk
 
 @pytest.mark.xfail
-def test_risk_deletion_with_one_risk_simulation():
-    simulation = setup_simulation(components=[generate_test_population, CategoricalRiskHandler(241, 'stunting')] 
-                                          + diarrhea_factory(), start=datetime(2005, 1, 1))
+def test_risk_deletion_with_one_risk():
+    simulation = setup_simulation(components=[generate_test_population,
+                                              CategoricalRiskHandler(241, 'stunting')]
+                                              + diarrhea_factory(),
+                                              start=datetime(2005, 1, 1))
 
     simulation = set_up_test_parameters(simulation, multiple_risks_test=False)
 
-    simulation = set_up_exposures(simulation, multiple_risks_test=False, full_exposure=False)
+    simulation = set_up_exposures(simulation, multiple_risks_test=False,
+                                  full_exposure=False)
 
-    rota_effective_inc = simulation.values.get_rate('incidence_rate.diarrhea_due_to_rotaviral_entiritis')
-    rota_inc_unexposed = rota_effective_inc(simulation.population.population.index)
+    rota_effective_inc = simulation.values.get_rate(
+        'incidence_rate.diarrhea_due_to_rotaviral_entiritis')
+    rota_inc_unexposed = rota_effective_inc(
+        simulation.population.population.index)
 
-    assert np.all(rota_inc_unexposed == 2.5), "risk deleted incidence rate should be equal to base rate * (1-PAF)." + \
-                                              "In the case of this test the answer should be 2.5. (10 * (1-.75))"
+    assert np.all(rota_inc_unexposed == 2.5), "risk deleted incidence rate" + \
+        "should be equal to base rate * (1-PAF). In the case of this test" + \
+        "the answer should be 2.5. That is, (10 * (1-.75)) = 2.5"
 
 
 @pytest.mark.xfail
-def test_that_rrs_applied_correctly_with_one_risk_simulation():
-    simulation = setup_simulation(components=[generate_test_population, CategoricalRiskHandler(241, 'stunting')]
-                                          + diarrhea_factory(), start=datetime(2005, 1, 1))
+def test_that_rrs_applied_correctly_with_one_risk():
+    simulation = setup_simulation(components=[generate_test_population,
+                                              CategoricalRiskHandler(241, 'stunting')]
+                                              + diarrhea_factory(),
+                                              start=datetime(2005, 1, 1))
 
     simulation = set_up_test_parameters(simulation, multiple_risks_test=False)
 
-    simulation = set_up_exposures(simulation, multiple_risks_test=False, full_exposure=True)
+    simulation = set_up_exposures(simulation, multiple_risks_test=False,
+                                  full_exposure=True)
 
-    rota_effective_inc = simulation.values.get_rate('incidence_rate.diarrhea_due_to_rotaviral_entiritis')
-    rota_inc_exposed = rota_effective_inc(simulation.population.population.index)
+    rota_effective_inc = simulation.values.get_rate(
+        'incidence_rate.diarrhea_due_to_rotaviral_entiritis')
+    rota_inc_exposed = rota_effective_inc(
+        simulation.population.population.index)
 
-    assert np.all(rota_inc_exposed == 5), "incidence rate should be equal to base rate * (1-PAF) * RR." + \
-                                            "In the case of this test the answer should be 5. (10 * (1-.75) * 2)"
-
-
-#############################################################################
-# Step 3. Conduct tests in a simulation that includes multiple risk factors #
-#############################################################################
+    assert np.all(rota_inc_exposed == 5), "incidence rate should be equal" + \
+        "to base rate * (1-PAF) * RR. In the case of this test the answer" + \
+        " should be 5. (10 * (1-.75) * 2)"
 
 
-def test_risk_deletion_with_multiple_risk_simulation():
-    simulation = setup_simulation(components=[generate_test_population, CategoricalRiskHandler(241, 'stunting'), CategoricalRiskHandler(84, 'unsafe_sanitation')] + diarrhea_factory(), start=datetime(2005, 1, 1))
+###########################################################################
+# Step 3. Conduct tests for a cause associated with multiple risk factors #
+###########################################################################
 
-    simulation = set_up_test_parameters(simulation, multiple_risks_test=True)
 
-    simulation = set_up_exposures(simulation, multiple_risks_test=True, full_exposure=False)
-
-    rota_effective_inc = simulation.values.get_rate('incidence_rate.diarrhea_due_to_rotaviral_entiritis')
-    rota_inc_unexposed = rota_effective_inc(simulation.population.population.index)
-
-    assert np.all(rota_inc_unexposed == 1.875), "risk deleted incidence rate should be equal to base rate * (1 - (1 - PAF1) * (1 - PAF2))." + "In the case of this test the answer should be 1.875. (10 * (1 - Joint PAF). Joint PAF = (1 - (1 - .75) * (1 - .25))"
-
-def test_that_rrs_applied_correctly_with_multiple_risk_simulation():
-    simulation = setup_simulation(components=[generate_test_population, CategoricalRiskHandler(241, 'stunting'), CategoricalRiskHandler(84, 'unsafe_sanitation')] + diarrhea_factory(), start=datetime(2005, 1, 1))
+def test_risk_deletion_with_multiple_risks():
+    simulation = setup_simulation(components=[generate_test_population,
+                                              CategoricalRiskHandler(241, 'stunting'),
+                                              CategoricalRiskHandler(84, 'unsafe_sanitation')]
+                                              + diarrhea_factory(),
+                                              start=datetime(2005, 1, 1))
 
     simulation = set_up_test_parameters(simulation, multiple_risks_test=True)
 
-    simulation = set_up_exposures(simulation, multiple_risks_test=True, full_exposure=True)
+    simulation = set_up_exposures(simulation, multiple_risks_test=True,
+                                  full_exposure=False)
 
-    rota_effective_inc = simulation.values.get_rate('incidence_rate.diarrhea_due_to_rotaviral_entiritis')
-    rota_inc_exposed = rota_effective_inc(simulation.population.population.index)
+    rota_effective_inc = simulation.values.get_rate(
+        'incidence_rate.diarrhea_due_to_rotaviral_entiritis')
+    rota_inc_unexposed = rota_effective_inc(
+        simulation.population.population.index)
 
-    assert np.all(rota_inc_exposed == 7.5), "risk deleted incidence rate should be equal to base rate * (1 - (1 - PAF1) * (1 - PAF2))." + "In the case of this test the answer should be 7.5. (10 * [1 - Joint PAF] * 2 * 2). Joint PAF = (1 - [1 - .75] * [1 - .25])"
+    assert np.all(rota_inc_unexposed == 1.875), "risk deleted incidence" + \
+        "should be equal to base rate * (1 - (1 - PAF1) * (1 - PAF2))." + \
+        "In the case of this test the answer should be 1.875." + \
+        "That is, (10 * (1 - Joint PAF) = 1.875, where" + \
+        "Joint PAF = (1 - (1 - .75) * (1 - .25))"
+
+
+def test_that_rrs_applied_correctly_with_multiple_risks():
+    simulation = setup_simulation(components=[generate_test_population,
+                                              CategoricalRiskHandler(241, 'stunting'),
+                                              CategoricalRiskHandler(84, 'unsafe_sanitation')]
+                                              + diarrhea_factory(),
+                                              start=datetime(2005, 1, 1))
+
+    simulation = set_up_test_parameters(simulation, multiple_risks_test=True)
+
+    simulation = set_up_exposures(simulation, multiple_risks_test=True,
+                                  full_exposure=True)
+
+    rota_effective_inc = simulation.values.get_rate(
+        'incidence_rate.diarrhea_due_to_rotaviral_entiritis')
+    rota_inc_exposed = rota_effective_inc(
+        simulation.population.population.index)
+
+    assert np.all(rota_inc_exposed == 7.5), "risk deleted incidence rate" + \
+        "should be equal to base rate * (1 - (1 - PAF1) * (1 - PAF2))." + \
+        "In the case of this test the answer should be 7.5." + \
+        "That is, (10 * [1 - Joint PAF] * 2 * 2) = 7.5." +\
+        "Where Joint PAF = (1 - [1 - .75] * [1 - .25])"
 
 # End.
