6c334745561c261b2851c7882028634c206067f1
James Collins <collijk@uw.edu>
2017-05-02 17:31:34 -0700

    PEP 8 compliance and some minor syntactic adjustments

--- ceam_tests/framework/test_randomness.py
+++ ceam_tests/framework/test_randomness.py
@@ -1,10 +1,10 @@
-import pytest
 from datetime import datetime
 
 import pandas as pd
 import numpy as np
 
 from ceam.framework.randomness import RandomnessStream, RESIDUAL_CHOICE
+
 
 def test_filter_for_probability():
     clock = [datetime(1990, 1, 1)]
@@ -20,6 +20,7 @@ def test_filter_for_probability():
     sub_sub_index = r.filter_for_probability(sub_index, 0.5)
     assert round(len(sub_sub_index)/len(sub_index), 1) == 0.5
 
+
 def test_choice__default_weights():
     clock = [datetime(1990, 1, 1)]
     r = RandomnessStream('test', lambda: clock[0], 1)
@@ -29,8 +30,9 @@ def test_choice__default_weights():
     chosen = r.choice(index, ['a', 'small', 'bird'])
 
     count = chosen.value_counts()
-    for k,c in count.items():
-        assert np.allclose(c/len(index), 1/3, atol=0.005)
+    for k, c in count.items():
+        assert np.abs(c/len(index) - 1/3) < 0.005
+
 
 def test_choice__homogenious_weights():
     clock = [datetime(1990, 1, 1)]
@@ -41,8 +43,9 @@ def test_choice__homogenious_weights():
     chosen = r.choice(index, ['a', 'small', 'bird'], [10, 10, 10])
 
     count = chosen.value_counts()
-    for k,c in count.items():
-        assert np.allclose(c/len(index), 1/3, atol=0.005)
+    for k, c in count.items():
+        assert np.abs(c / len(index) - 1 / 3) < 0.005
+
 
 def test_choice__hetrogenious_weights():
     clock = [datetime(1990, 1, 1)]
@@ -56,7 +59,8 @@ def test_choice__hetrogenious_weights():
 
     count = chosen.value_counts()
     for c, p in zip(choices, weights):
-        assert np.allclose(count[c]/len(index), p, atol=0.005)
+        assert np.abs(count[c]/len(index) - p) < 0.005
+
 
 def test_choice__residual_choice():
     clock = [datetime(1990, 1, 1)]
@@ -71,4 +75,4 @@ def test_choice__residual_choice():
     count = chosen.value_counts()
     expected_weights = [0.2, 0.1, 0.7]
     for c, p in zip(choices, expected_weights):
-        assert np.allclose(count[c]/len(index), p, atol=0.005)
+        assert np.abs(count[c]/len(index) - p) < 0.005
