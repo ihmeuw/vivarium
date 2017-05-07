e72ca77f08a6be969cc8b882168268a5271056be
emumford <emumford@uw.edu>
2017-05-07 12:28:25 -0700

    updating conftest to work with new configurations

--- ceam_tests/framework/test_risks/conftest.py
+++ ceam_tests/framework/test_risks/conftest.py
@@ -1,7 +1,7 @@
 from ceam import config
 
-config.set('simulation_parameters', 'initial_age', '0')
-config.set('simulation_parameters', 'pop_age_start', '0')
-config.set('simulation_parameters', 'pop_age_end', '0')
-config.set('simulation_parameters', 'year_start', '2005')
+config.simulation_parameters.initial_age = 0
+config.simulation_parameters.pop_age_start = 0
+config.simulation_parameters.pop_age_end = 0
+config.simulation_parameters.year_start = 2005
 
