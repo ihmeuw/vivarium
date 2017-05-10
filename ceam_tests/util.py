aebb6451776f7f4b382b418aa267c91c65d83f14
emumford <emumford@uw.edu>
2017-05-10 10:46:53 -0700

    updated setup simulation to allow for us to pass in an initial age of 0

--- ceam_tests/util.py
+++ ceam_tests/util.py
@@ -23,7 +23,7 @@ def setup_simulation(components, population_size = 100):
 
     simulation.current_time = datetime(year_start, 1, 1)
 
-    if config.simulation_parameters.initial_age:
+    if config.simulation_parameters.initial_age is not None:
         simulation.population._create_simulants(population_size, population_configuration={'initial_age': config.simulation_parameters.initial_age})
     else:
         simulation.population._create_simulants(population_size)
