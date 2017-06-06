54c32aa55aa8a476e9912d05affec1e1e3495e11
James Collins <collijk@uw.edu>
2017-06-05 18:24:43 -0700

    Removed extraneous files

--- ceam_tests/framework/test_risks/conftest.py
+++
@@ -1,7 +0,0 @@
-from ceam import config
-
-config.simulation_parameters.initial_age = 0
-config.simulation_parameters.pop_age_start = 0
-config.simulation_parameters.pop_age_end = 0
-config.simulation_parameters.year_start = 2005
-
