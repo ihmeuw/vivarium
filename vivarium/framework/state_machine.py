15442e6a7b0f3b2693fb691487b55b09772facd0
James Collins <collijk@uw.edu>
2017-08-02 11:59:15 -0700

    Fixed bug with empty transition indexes

--- vivarium/framework/state_machine.py
+++ vivarium/framework/state_machine.py
@@ -327,7 +327,7 @@ class TransitionSet(list):
         """
         outputs = list(outputs)
         total = np.sum(probabilities, axis=1)
-        if self.allow_null_transition:
+        if self.allow_null_transition or not np.any(total):
             if np.any(total > 1+1e-08):  # Accommodate rounding errors
                 raise ValueError(
                     "Null transition requested with un-normalized probability weights: {}".format(probabilities))
