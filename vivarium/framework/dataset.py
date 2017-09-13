5854cbc82091b8ceeb5d52b35cf21f53d75cd3c3
Alec Deason <alecwd@uw.edu>
2017-09-13 14:25:18 -0700

    Temporarily remove dataset.py so we can merge the rest of this code into develop

--- vivarium/framework/dataset.py
+++
@@ -1,7 +0,0 @@
-class Placeholder:
-    def __init__(self, entity_path):
-        self.entity_path = entity_path
-
-class DatasetManager:
-    def construct_data_container(self, entity_path):
-        raise NotImplementedError()
