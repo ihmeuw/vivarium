dce9d3f8242f90f6262ce2152edc4439b9ff4cd8
James Collins <collijk@uw.edu>
2018-04-22 21:18:50 -0700

    arranging some configuration stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,26 +2,22 @@ import os
 
 import pytest
 
-from vivarium.framework.engine import build_simulation_configuration
+from vivarium.framework.configuration import build_simulation_configuration
 
 
 @pytest.fixture(scope='module')
 def base_config():
-    config = build_simulation_configuration().configuration
+    config = build_simulation_configuration()
     metadata = {'layer': 'override', 'source': os.path.realpath(__file__)}
-    config.reset_layer('override', preserve_keys=['input_data.intermediary_data_cache_path',
-                                                  'input_data.auxiliary_data_folder'])
-    config.update(
-        {
-            'time': {
-                'start': {
-                    'year': 1990,
-                },
-                'end': {
-                    'year': 2010
-                },
-                'step_size': 30.5
-            }
-        }, **metadata)
-
+    config.update({
+        'time': {
+            'start': {
+                'year': 1990,
+            },
+            'end': {
+                'year': 2010
+            },
+            'step_size': 30.5
+        }
+    }, **metadata)
     return config
