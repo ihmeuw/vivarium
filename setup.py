56d15616b9c5da733bae3973f0d1f57321df186b
James Collins <collijk@uw.edu>
2018-07-24 09:35:19 -0700

    Last manual version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivarium',
-    version='0.5',
+    version='0.8.0',
     packages=find_packages(),
     include_package_data=True,
     entry_points="""
