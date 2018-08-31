c2a91c427f4c890ed2a03c3fde99cecd6c55daba
James Collins <collijk@uw.edu>
2018-08-31 16:02:25 +1000

    Update some docstrings and Base pop tutorial

---
+++ src/vivarium/examples/disease_model/__init__.py
@@ -0,0 +1,4 @@
+from .population import BasePopulation, Mortality, Observer
+from .disease import DiseaseModel, DiseaseState, DiseaseTransition, SIS_DiseaseModel
+from .risk import Risk, DirectEffect
+from .intervention import MagicWandIntervention
