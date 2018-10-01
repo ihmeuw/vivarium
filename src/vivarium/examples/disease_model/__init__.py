1067b2124bb0067d9a6639d47ae8ad69326fac15
James Collins <collijk@uw.edu>
2018-10-01 08:58:33 -0700

    Added convenience method for getting example disease model simulation.

--- src/vivarium/examples/disease_model/__init__.py
+++ src/vivarium/examples/disease_model/__init__.py
@@ -10,3 +10,9 @@ def get_model_specification_path():
     from pathlib import Path
     p = Path(__file__).parent / 'disease_model.yaml'
     return str(p)
+
+
+def get_disease_model_simulation():
+    from vivarium.interface import setup_simulation_from_model_specification
+    p = get_model_specification_path()
+    return setup_simulation_from_model_specification(p)
