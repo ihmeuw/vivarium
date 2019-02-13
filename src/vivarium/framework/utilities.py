2cfd0bdd147ff506a9c6b2e7c21fd228c4c19554
Cody <chorst@uw.edu>
2019-02-13 11:14:36 -0800

    cap rate at 250

--- src/vivarium/framework/utilities.py
+++ src/vivarium/framework/utilities.py
@@ -13,6 +13,10 @@ def to_yearly(value, time_step):
 
 
 def rate_to_probability(rate):
+    # encountered underflow from rate > 30k
+    # for rates greater than 250, exp(-rate) evaluates to 1e-109
+    # beware machine-specific floating point issues
+    rate[rate > 250] = 250.0
     return 1-np.exp(-rate)
 
 
