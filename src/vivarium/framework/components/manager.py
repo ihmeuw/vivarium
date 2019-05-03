2ca2c41b8247898e62235a3d038e29f9df1f7674
Cody <chorst@uw.edu>
2019-05-03 09:47:51 -0700

    beef up docstrings

--- src/vivarium/framework/components/manager.py
+++ src/vivarium/framework/components/manager.py
@@ -1,3 +1,12 @@
+"""This module contains the interface and the default implementation of the
+``vivarium`` component manager system.
+
+The component manager is a plugin that manages all of the components in a
+simulation and is responsible for the ``setup`` phase of their lifecycle.  It
+maintains a distinction between the top-level (or "manager") components that are
+core to the framework's operation and lower-level components that are
+simulation-specific. This is the only dependency management currently provided.
+"""
 import inspect
 from typing import Sequence, Any
 
@@ -10,9 +19,8 @@ class ComponentConfigError(VivariumError):
 
 
 class ComponentManager:
-    """Responsible for tracking all components in a Vivarium simulation, enabling
-    access to them, and for initiating the ``setup`` life-cycle stage of each
-    component."""
+    """Handles tracking and access patterns for all components in a Vivarium
+    and initiating the ``setup`` life-cycle stage of each component."""
 
     def __init__(self):
         self._managers = []
@@ -30,7 +38,7 @@ class ComponentManager:
         self._add_components(self._managers, managers)
 
     def add_components(self, components: Sequence):
-        """Register new components with the system. Components are setup after
+        """Registers new components with the system. Components are setup after
         managers.
 
         Parameters
@@ -55,7 +63,7 @@ class ComponentManager:
         Parameters
         ----------
         component_type
-            A component class.
+            A component type.
         Returns
         -------
             A list of components.
@@ -63,8 +71,9 @@ class ComponentManager:
         return [c for c in self._components if isinstance(c, component_type)]
 
     def setup_components(self, builder, configuration: ConfigTree):
-        """Apply component-level configuration defaults to the global configuration
-        and run setup methods. Runs first on managers, then on components.
+        """Apply component-level configuration defaults to the global
+        configuration and run setup methods. Runs first on managers, then on
+        components.
 
         This can result in new components due to side effects of setup.
 
@@ -80,7 +89,8 @@ class ComponentManager:
 
 
 class ComponentInterface:
-    """The component manager interface available from the builder."""
+    """The component manager interface made available from the builder."""
+
     def __init__(self, component_manager: ComponentManager):
         self._component_manager = component_manager
 
@@ -100,7 +110,33 @@ class ComponentInterface:
 
 
 def _setup_components(builder, component_list: Sequence, configuration: ConfigTree) -> Sequence:
+    """Helper method for configuring and setting up a list of components.
 
+    This function first loops over `component_list` and applies configuration
+    defaults if present.  The application entails updating the `configuration`
+    object. Then, the list is looped over until empty, with components being
+    popped and setup. Because of the side effect below, new components can be
+    added. This necessitates the while loop and also another check of
+    configuration.
+
+    Importantly, this function is called by the ComponentManager and it is
+    passed a a reference to one of the lists the Component manager holds. This
+    list is also accessible through the interface on the builder, so a
+    component's setup method can mutate the list.
+
+    Parameters
+    ----------
+    builder
+        Interface to several simulation tools.
+    component_list
+        A list of vivarium components
+    configuration
+        A vivarium configuration object
+
+    Returns
+    -------
+        A list of components which have been configured and setup.
+    """
     configured = []
     for c in component_list:  # apply top-level configurations first
         if hasattr(c, "configuration_defaults") and not c in configured:
@@ -126,7 +162,19 @@ def _setup_components(builder, component_list: Sequence, configuration: ConfigTr
     return setup
 
 
-def _apply_component_default_configuration(configuration, component):
+def _apply_component_default_configuration(configuration: ConfigTree, component: Any):
+    """Checks if a default configuration attached to a component is duplicated in
+    the current config tree, which will raise. If it is not, the configuration
+    is applied.
+
+    Parameters
+    ----------
+    configuration
+        A vivarium configuration object
+    component
+        A vivarium component
+    """
+
     # This reapplies configuration from some components but it is idempotent so there's no effect.
     if component.__module__ == '__main__':
         # This is defined directly in a script or notebook so there's no file to attribute it to
@@ -137,7 +185,26 @@ def _apply_component_default_configuration(configuration, component):
     configuration.update(component.configuration_defaults, layer='component_configs', source=source)
 
 
-def _check_duplicated_default_configuration(component, config, source):
+def _check_duplicated_default_configuration(component: Any, config: ConfigTree, source: str):
+    """Checks that the keys present in a component's default configuration are
+    not already present in the configtree. Source is just for making a cogent
+    error message.
+
+    Parameters
+    ----------
+    component
+        A vivarium component
+    config
+        A vivarium configuration object
+    source
+        The file containing the code that describes the component
+
+    Raises
+    -------
+    ComponentConfigError
+        A component's default configuration is already present in the config
+        tree.
+    """
     overlapped = set(component.keys()).intersection(config.keys())
     if not overlapped:
         pass
