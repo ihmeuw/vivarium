53a4abb8941f97ffafa439fed06239ff872d5845
James Collins <collijk@uw.edu>
2019-05-07 10:25:37 -0700

    Missing comma

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         'sphinx',
         'sphinx-autodoc-typehints',
         'sphinx-rtd-theme',
-        'sphinx-click'
+        'sphinx-click',
         'IPython',
         'matplotlib'
     ]
