d13bdafda595fd22f9ff2c6391af9daa517fd73a
Cody <chorst@uw.edu>
2019-05-31 12:13:32 -0700

    introduce component type

--- src/vivarium/framework/components/__init__.py
+++ src/vivarium/framework/components/__init__.py
@@ -1,2 +1,5 @@
+from typing import Type
 from .manager import ComponentManager, ComponentInterface, ComponentConfigError
 from .parser import ComponentConfigurationParser
+
+ComponentType = Type  # human readable type
