f52cddf94cd6017491acb9d4065e526038817160
Cody <chorst@uw.edu>
2019-08-05 16:39:32 -0700

    fix typing and docstring wrap

--- src/vivarium/framework/lookup.py
+++ src/vivarium/framework/lookup.py
@@ -10,6 +10,7 @@ population index and returns data specific to the individuals represented by
 that index. See the :ref:`lookup concept note <lookup_concept>` for more.
 """
 from numbers import Number
+from typing import Iterable, Union
 from datetime import datetime, timedelta
 from typing import Union, List, Tuple, Callable, TypeVar
 
@@ -22,7 +23,8 @@ ScalarValue = TypeVar('ScalarValue', Number, timedelta, datetime)
 
 
 class InterpolatedTable:
-    """A callable that returns the result of an interpolation function over input data.
+    """A callable that returns the result of an interpolation function over
+    input data.
 
     Attributes
     ----------
@@ -30,15 +32,16 @@ class InterpolatedTable:
         The data from which to build the interpolation. Contains the set of
         key_columns, parameter_columns, and value_columns.
     population_view :
-        View of the population to be used when the table is called with an index.
+        View of the population to be used when the table is called with an
+        index.
     key_columns :
         Column names to be used as categorical parameters in Interpolation
         to select between interpolation functions.
     parameter_columns :
         Column names to be used as continuous parameters in Interpolation.
     value_columns :
-        Names of value columns to be interpolated over. All non parameter- and key-
-        columns in data.
+        Names of value columns to be interpolated over. All non parameter- and
+        key- columns in data.
     interpolation_order :
         Order of interpolation.
     clock :
@@ -48,7 +51,8 @@ class InterpolatedTable:
 
     Notes
     -----
-    These should not be created directly. Use the `lookup` method on the builder during setup.
+    These should not be created directly. Use the `lookup` method on the builder
+    during setup.
     """
     def __init__(self, data: pd.DataFrame, population_view: PopulationView, key_columns: Union[List[str], Tuple[str]],
                  parameter_columns: ParameterType, value_columns: Union[List[str], Tuple[str]],
@@ -70,12 +74,11 @@ class InterpolatedTable:
 
         Parameters
         ----------
-        index :
+        index
             Index of the population to interpolate for.
 
         Returns
         -------
-        pd.DataFrame
             A table with the interpolated values for the population requested.
         """
 
@@ -94,37 +97,40 @@ class InterpolatedTable:
 
 
 class ScalarTable:
-    """A callable that returns a series or dataframe from a single value or list of values with no interpolation.
+    """A callable that returns a series or dataframe from a single value or list
+    of values with no interpolation.
 
     Attributes
     ----------
-    values :
+    values
         The scalar value(s) from which to build table columns.
-    value_columns :
+    value_columns
         List of string names to be used to name the columns of the table built
         from values.
 
     Notes
     -----
-    These should not be created directly. Use the `lookup` method on the builder during setup.
+    These should not be created directly. Use the `lookup` method on the builder
+    during setup.
     """
     def __init__(self, values: Union[List[ScalarValue], Tuple[ScalarValue]], value_columns: Union[List[str], Tuple[str]]):
 
         self.values = values
         self.value_columns = value_columns
 
-    def __call__(self, index) -> pd.DataFrame:
-        """Build a table with ``index`` and columns for each of the scalar values.
+    def __call__(self, index: pd.Index) -> pd.DataFrame:
+        """Build a table with ``index`` and columns for each of the scalar
+        values.
 
         Parameters
         ----------
-        index :
+        index
             Index of the population to construct table for.
 
         Returns
         -------
-        pd.DataFrame
-            A table with a column for each of the scalar values for the population requested.
+            A table with a column for each of the scalar values for the
+            population requested.
         """
         if not isinstance(self.values, (list, tuple)):
             values = pd.Series(self.values, index=index, name=self.value_columns[0] if self.value_columns else None)
@@ -146,7 +152,8 @@ class LookupTable:
 
     Notes
     -----
-    These should not be created directly. Use the `lookup` method on the builder during setup.
+    These should not be created directly. Use the `lookup` method on the builder
+    during setup.
     """
     def __init__(self, data: Union[ScalarValue, pd.DataFrame, List[ScalarValue], Tuple[ScalarValue]],
                  population_view: Callable, key_columns: Union[List[str], Tuple[str]],
@@ -164,18 +171,18 @@ class LookupTable:
             self._table = InterpolatedTable(data, population_view(view_columns), key_columns,
                                             parameter_columns, value_columns, interpolation_order, clock, extrapolate)
 
-    def __call__(self, index) -> Union[pd.DataFrame, pd.Series]:
+    def __call__(self, index: pd.Index) -> Union[pd.DataFrame, pd.Series]:
         """Get the interpolated or scalar table values for the given index.
 
         Parameters
         ----------
-        index : Index
-              Index for population view
+        index
+            Index for population view
 
         Returns
         -------
-        pandas.Series if interpolated or scalar values for index are one column,
-        pandas.DataFrame if multiple columns
+            pandas.Series if interpolated or scalar values for index are one
+            column, pandas.DataFrame if multiple columns
         """
         table_view = self._table(index)
         if len(table_view.columns) == 1:
@@ -219,8 +226,8 @@ class LookupTableManager:
 
     Notes
     -----
-    Client code should never access this class directly. Use ``lookup`` on the builder during setup
-    to get references to LookupTable objects.
+    Client code should never access this class directly. Use ``lookup`` on the
+    builder during setup to get references to LookupTable objects.
     """
 
     configuration_defaults = {
@@ -253,38 +260,44 @@ class LookupTableInterface:
     def __init__(self, manager):
         self._lookup_table_manager = manager
 
-    def build_table(self, data, key_columns=('sex',), parameter_columns=(['age', 'age_group_start', 'age_group_end'],
-                                                                         ['year', 'year_start', 'year_end']),
-                    value_columns=None) -> LookupTable:
+    def build_table(self, data,
+                    key_columns: Iterable[str]=('sex',),
+                    parameter_columns: Iterable[Union[str, Iterable[str]]]=(['age', 'age_group_start', 'age_group_end'],
+                                                                            ['year', 'year_start', 'year_end']),
+                    value_columns: Iterable[str]=None) -> LookupTable:
         """Construct a LookupTable from input data.
 
-        If data is a ``pandas.DataFrame``, an interpolation function of the specified
-        order will be calculated for each permutation of the set of key_columns.
-        The columns in parameter_columns will be used as parameters for the interpolation
-        functions which will estimate all remaining columns in the table.
+        If data is a ``pandas.DataFrame``, an interpolation function of the
+        specified order will be calculated for each permutation of the set of
+        key_columns. The columns in parameter_columns will be used as parameters
+        for the interpolation functions which will estimate all remaining
+        columns in the table.
 
-        If data is a number, time, list, or tuple, a scalar table will be constructed with
-        the values in data as the values in each column of the table, named according to
-        value_columns.
+        If data is a number, time, list, or tuple, a scalar table will be
+        constructed with the values in data as the values in each column of the
+        table, named according to value_columns.
 
 
         Parameters
         ----------
-        data        : The source data which will be used to build the resulting LookupTable.
-        key_columns : [str]
-                      Columns used to select between interpolation functions. These
-                      should be the non-continuous variables in the data. For
-                      example 'sex' in data about a population.
-        parameter_columns : [str]
-                      The columns which contain the parameters to the interpolation functions.
-                      These should be the continuous variables. For example 'age'
-                      in data about a population.
-        value_columns : [str]
-                      The data columns that will be in the resulting LookupTable. Columns to be
-                      interpolated over if interpolation or the names of the columns in the scalar
-                      table.
+        data
+            The source data which will be used to build the resulting
+            LookupTable.
+        key_columns
+            Columns used to select between interpolation functions. These should
+            be the non-continuous variables in the data. For example 'sex' in
+            data about a population.
+        parameter_columns
+            The columns which contain the parameters to the interpolation
+            functions. These should be the continuous variables. For example
+            'age' in data about a population.
+        value_columns
+            The data columns that will be in the resulting LookupTable. Columns
+            to be interpolated over if interpolation or the names of the columns
+            in the scalar table.
+
         Returns
         -------
-        LookupTable
+            LookupTable
         """
         return self._lookup_table_manager.build_table(data, key_columns, parameter_columns, value_columns)
