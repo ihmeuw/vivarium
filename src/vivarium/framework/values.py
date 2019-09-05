bec518c4a3f3b04d9241abd94c8662af269a7130
James Collins <collijk@uw.edu>
2019-09-04 20:36:07 -0700

    Values docs formatting

--- src/vivarium/framework/values.py
+++ src/vivarium/framework/values.py
@@ -27,40 +27,34 @@ _log = logging.getLogger(__name__)
 
 
 class DynamicValueError(VivariumError):
-    """Indicates that a value was invoked without being properly configured.
-    i.e. no source specified.
-    """
+    """Indicates and improperly configured value was invoked."""
     pass
 
 
 def replace_combiner(value, mutator, *args, **kwargs):
-    """Replaces the output of the source or mutator with the output
-    of the subsequent mutator. This is the default combiner.
+    """Replace the previous pipeline output with the output of the mutator.
+
+    This is the default combiner.
+
     """
     args = list(args) + [value]
     return mutator(*args, **kwargs)
 
 
 def set_combiner(value, mutator, *args, **kwargs):
-    """Expects the output of the source to be a set to which
-    the result of each mutator is added.
-    """
+    """Aggregates source and mutator output into a set."""
     value.add(mutator(*args, **kwargs))
     return value
 
 
 def list_combiner(value, mutator, *args, **kwargs):
-    """Expects the output of the source to be a list to which
-    the result of each mutator is appended.
-    """
+    """Aggregates source and mutator output into a list."""
     value.append(mutator(*args, **kwargs))
     return value
 
 
 def rescale_post_processor(a, time_step):
-    """Assumes that the value is an annual rate and rescales it to the
-    current time step.
-    """
+    """Rescales annual rates to time-step appropriate rates."""
     return from_yearly(a, pd.Timedelta(time_step, unit='D'))
 
 
@@ -132,11 +126,11 @@ class ValuesManager:
 
     Notes
     -----
-    Client code should never need to interact with this class
-    except through the dynamic value and rate constructors exposed
-    via the builder during the setup phase.
-    """
+        Client code should never need to interact with this class
+        except through the dynamic value and rate constructors exposed
+        via the builder during the setup phase.
 
+    """
     def __init__(self):
         self._pipelines = defaultdict(Pipeline)
 
@@ -156,7 +150,7 @@ class ValuesManager:
                                                                        'simulation_end', 'report'])
 
     def on_post_setup(self, event):
-        # FIXME: This should raise an error, but can't due to risk effect generation
+        # FIXME: This should raise an error, but can't due to downstream dependants.
         _log.debug(f"Unsourced pipelines: {[p for p, v in self._pipelines.items() if not v.source]}")
 
     def register_value_modifier(self, value_name, modifier):
@@ -209,55 +203,61 @@ class ValuesInterface:
     def __init__(self, value_manager: ValuesManager):
         self._value_manager = value_manager
 
-    def register_value_producer(self, value_name: str, source: Callable[..., pd.DataFrame]=None,
-                                preferred_combiner: Callable=replace_combiner,
-                                preferred_post_processor: Callable[..., pd.DataFrame]=None) -> Callable:
+    def register_value_producer(self, value_name: str, source: Callable[..., pd.DataFrame] = None,
+                                preferred_combiner: Callable = replace_combiner,
+                                preferred_post_processor: Callable[..., pd.DataFrame] = None) -> Callable:
         """Marks a ``Callable`` as the producer of a named value.
 
         Parameters
         ----------
-        value_name :
+        value_name
             The name of the new dynamic value pipeline.
-        source :
+        source
             A callable source for the dynamic value pipeline.
-        preferred_combiner :
-            A strategy for combining the source and the results of any calls to mutators in the pipeline.
-            ``vivarium`` provides the strategies ``replace_combiner`` (the default), ``list_combiner``,
-            and ``set_combiner`` which are importable from ``vivarium.framework.values``.  Client code
-            may define additional strategies as necessary.
-        preferred_post_processor :
-            A strategy for processing the final output of the pipeline. ``vivarium`` provides the strategies
-            ``rescale_post_processor`` and ``joint_value_post_processor`` which are importable from
-            ``vivarium.framework.values``.  Client code may define additional strategies as necessary.
+        preferred_combiner
+            A strategy for combining the source and the results of any calls
+            to mutators in the pipeline. ``vivarium`` provides the strategies
+            ``replace_combiner`` (the default), ``list_combiner``, and
+            ``set_combiner`` which are importable from
+            ``vivarium.framework.values``.  Client code may define additional
+            strategies as necessary.
+        preferred_post_processor
+            A strategy for processing the final output of the pipeline.
+            ``vivarium`` provides the strategies ``rescale_post_processor``
+            and ``joint_value_post_processor`` which are importable from
+            ``vivarium.framework.values``.  Client code may define additional
+            strategies as necessary.
 
         Returns
         -------
-        Callable
             A callable reference to the named dynamic value pipeline.
+
         """
         return self._value_manager.register_value_producer(value_name, source,
                                                            preferred_combiner,
                                                            preferred_post_processor)
 
-    def register_rate_producer(self, rate_name: str, source: Callable[..., pd.DataFrame]=None) -> Callable:
+    def register_rate_producer(self, rate_name: str, source: Callable[..., pd.DataFrame] = None) -> Callable:
         """Marks a ``Callable`` as the producer of a named rate.
 
-        This is a convenience wrapper around ``register_value_producer`` that makes sure
-        rate data is appropriately scaled to the size of the simulation time step.
-        It is equivalent to ``register_value_producer(value_name, source,
-        preferred_combiner=replace_combiner, preferred_post_processor=rescale_post_processor)``
+        This is a convenience wrapper around ``register_value_producer`` that
+        makes sure rate data is appropriately scaled to the size of the
+        simulation time step.  It is equivalent to
+        ``register_value_producer(value_name, source,
+        preferred_combiner=replace_combiner,
+        preferred_post_processor=rescale_post_processor)``
 
         Parameters
         ----------
-        rate_name :
+        rate_name
             The name of the new dynamic rate pipeline.
-        source :
+        source
             A callable source for the dynamic rate pipeline.
 
         Returns
         -------
-        Callable
             A callable reference to the named dynamic rate pipeline.
+
         """
         return self._value_manager.register_rate_producer(rate_name, source)
 
@@ -269,16 +269,14 @@ class ValuesInterface:
         value_name :
             The name of the dynamic value pipeline to be modified.
         modifier :
-            A function that modifies the source of the dynamic value pipeline when called.
-            If the pipeline has a ``replace_combiner``, the modifier should accept the same
-            arguments as the pipeline source with an additional last positional argument
-            for the results of the previous stage in the pipeline. For the ``list_combiner`` and
-            ``set_combiner`` strategies, the pipeline modifiers should have the same signature
-            as the pipeline source.
-        priority : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-            An indication of the order in which pipeline modifiers should be called. Modifiers with
-            smaller priority values will be called earlier in the pipeline.  Modifiers with
-            the same priority have no guaranteed ordering, and so should be commutative.
+            A function that modifies the source of the dynamic value pipeline
+            when called. If the pipeline has a ``replace_combiner``, the
+            modifier should accept the same arguments as the pipeline source
+            with an additional last positional argument for the results of the
+            previous stage in the pipeline. For the ``list_combiner`` and
+            ``set_combiner`` strategies, the pipeline modifiers should have
+            the same signature as the pipeline source.
+
         """
         self._value_manager.register_value_modifier(value_name, modifier)
 
