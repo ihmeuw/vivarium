2178b230729faaca4a0162dfe25395c6f96f13a0
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2021-10-21 17:18:22 -0700

    update example to be used in ACM COMPASS paper

--- src/vivarium/examples/disease_model/__init__.py
+++ src/vivarium/examples/disease_model/__init__.py
@@ -1,7 +1,7 @@
 from .population import BasePopulation
 from .mortality import Mortality
 from .observer import Observer
-from .disease import DiseaseModel, DiseaseState, DiseaseTransition, SIS_DiseaseModel
+from .disease import DiseaseModel, DiseaseState, DiseaseTransition, SISDiseaseModel
 from .risk import Risk, DirectEffect
 from .intervention import MagicWandIntervention
 
