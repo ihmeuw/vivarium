4b4d8acf119e5c61bb7bfa4782c1921a8f52609f
James Collins <collijk@uw.edu>
2021-10-25 08:18:19 -0700

    Add networkx to intersphinx mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -189,7 +189,8 @@ texinfo_documents = [
 intersphinx_mapping = {'python': ('https://docs.python.org/3.8', None),
                        'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
                        'tables': ('https://www.pytables.org/', None),
-                       'numpy': ('https://numpy.org/doc/stable/', None)}
+                       'numpy': ('https://numpy.org/doc/stable/', None),
+                       'networkx': ('https://networkx.org/documentation/stable/', None)}
 
 
 # -- Autodoc configuration ------------------------------------------------
