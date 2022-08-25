a74319c12ef10b74157b56f8a4e72a881243a64f
Matthew Kappel <mkappel@uw.edu>
2022-08-25 16:40:39 -0700

    Satisfy test_engine, black/isort

--- src/vivarium/framework/results/context.py
+++ src/vivarium/framework/results/context.py
@@ -15,18 +15,22 @@ class ResultsContext:
 
     def __init__(self):
         self._default_stratifications = []  # type: List[str]
-        self._stratifications = []        # type: List[Stratification]
+        self._stratifications = []  # type: List[Stratification]
         # keys are event names
         # values are dicts with key (filter, grouper) value (measure, aggregator, additional_keys)
         self._observations = defaultdict(lambda: defaultdict(list))
 
     def set_default_stratifications(self, default_grouping_columns: List[str]):
         if self._default_stratifications:
-            raise ResultsConfigurationError('Multiple calls are being made to set default grouping columns '
-                                            'for results production.')
+            raise ResultsConfigurationError(
+                "Multiple calls are being made to set default grouping columns "
+                "for results production."
+            )
         if not default_grouping_columns:
-            raise ResultsConfigurationError('Attempting to set an empty list as the default grouping columns '
-                                            'for results production.')
+            raise ResultsConfigurationError(
+                "Attempting to set an empty list as the default grouping columns "
+                "for results production."
+            )
         self._default_grouping_columns = default_grouping_columns
 
     def add_stratification(
@@ -35,7 +39,7 @@ class ResultsContext:
         sources: List[str],
         categories: List[str],
         mapper: Callable,
-        is_vectorized: bool
+        is_vectorized: bool,
     ):
         # TODO: implement this with stratifications
         # self._stratifications.append(
@@ -55,8 +59,9 @@ class ResultsContext:
     ):
         groupers = self._get_groupers(additional_stratifications, excluded_stratifications)
         (
-            self._producers[when][(pop_filter, groupers)]
-            .append((name, aggregator, additional_keys))
+            self._producers[when][(pop_filter, groupers)].append(
+                (name, aggregator, additional_keys)
+            )
         )
 
     def gather_results(self, population: pd.DataFrame, event_name: str) -> Dict[str, float]:
@@ -79,7 +84,7 @@ class ResultsContext:
     def _get_groupers(
         self,
         additional_stratifications: List[str] = (),
-        excluded_stratifications: List[str] = ()
+        excluded_stratifications: List[str] = (),
     ) -> Tuple[str, ...]:
         groupers = list(
             set(self._default_stratifications) - set(excluded_stratifications)
@@ -89,24 +94,31 @@ class ResultsContext:
         return tuple(sorted(groupers))
 
     @staticmethod
-    def _format_results(measure: str, aggregates: pd.DataFrame, **additional_keys: str) -> Dict[str, float]:
+    def _format_results(
+        measure: str, aggregates: pd.DataFrame, **additional_keys: str
+    ) -> Dict[str, float]:
         results = {}
         # First we expand the categorical index over unobserved pairs.
         # This ensures that the produced results are always the same length.
-        idx = pd.MultiIndex.from_product(aggregates.index.levels, names=aggregates.index.names)
+        idx = pd.MultiIndex.from_product(
+            aggregates.index.levels, names=aggregates.index.names
+        )
         data = pd.Series(data=0, index=idx)
         data.loc[aggregates.index] = aggregates
 
         def _format(field, param):
             """Format of the measure identifier tokens into FIELD_param."""
-            return f'{str(field).upper()}_{param}'
+            return f"{str(field).upper()}_{param}"
 
         for params, val in data.iteritems():
-            key = '_'.join(
-                [_format('measure', measure)]
+            key = "_".join(
+                [_format("measure", measure)]
                 + [_format(field, measure) for field, param in zip(data.index.names, params)]
                 # Sorts additional_keys by the field name.
-                + [_format(field, measure) for field, param in sorted(additional_keys.items())]
+                + [
+                    _format(field, measure)
+                    for field, param in sorted(additional_keys.items())
+                ]
             )
             results[key] = val
         return results
--- src/vivarium/framework/results/manager.py
+++ src/vivarium/framework/results/manager.py
@@ -3,7 +3,8 @@ from typing import Callable, List, Union
 
 import pandas as pd
 
-from vivarium.framework.engine import Builder
+# TODO: importing Builder causes circular import, fix that in implementation
+# from vivarium.framework.engine import Builder
 from vivarium.framework.event import Event
 from vivarium.framework.results.context import ResultsContext
 
@@ -20,49 +21,55 @@ class ResultsManager:
     stratifications and observations. It is able to record observations at
     any of the time-step sub-steps (`time_step__prepare`, `time_step`,
     `time_step__cleanup`, and `collect_metrics`).
-
     """
 
     def __init__(self):
         self._metrics = Counter()
         self._results_context = ResultsContext()
-        self._required_columns = {'tracked'}
+        self._required_columns = {"tracked"}
         self._required_values = set()
+        self._name = "results_manager"
+
+    @property
+    def name(self) -> str:
+        """The name of this ResultsManager."""
+        return self._name
 
     @property
     def metrics(self):
         return self._metrics.copy()
 
-    def setup(self, builder: 'Builder'):
+    def setup(self, builder):
         self.population_view = builder.population.get_view([])
         self.clock = builder.time.clock()
         self.step_size = builder.time.step_size()
 
-        builder.event.register_listener('time_step__prepare', self.on_time_step_prepare)
-        builder.event.register_listener('time_step', self.on_time_step)
-        builder.event.register_listener('time_step__cleanup', self.on_time_step_cleanup)
-        builder.event.register_listener('collect_metrics', self.on_collect_metrics)
+        builder.event.register_listener("time_step__prepare", self.on_time_step_prepare)
+        builder.event.register_listener("time_step", self.on_time_step)
+        builder.event.register_listener("time_step__cleanup", self.on_time_step_cleanup)
+        builder.event.register_listener("collect_metrics", self.on_collect_metrics)
 
         self.get_value = builder.value.get_value
 
-        builder.value.register_value_modifier('metrics', self.get_results)
+        builder.value.register_value_modifier("metrics", self.get_results)
 
     def on_time_step_prepare(self, event: Event):
-        self.gather_results('time_step__prepare', event)
+        self.gather_results("time_step__prepare", event)
 
     def on_time_step(self, event: Event):
-        self.gather_results('time_step', event)
+        self.gather_results("time_step", event)
 
     def on_time_step_cleanup(self, event: Event):
-        self.gather_results('time_step__cleanup', event)
+        self.gather_results("time_step__cleanup", event)
 
     def on_collect_metrics(self, event: Event):
-        self.gather_results('collect_metrics', event)
+        self.gather_results("collect_metrics", event)
 
     def gather_results(self, event_name: str, event: Event):
-        population = self._prepare_population(event)
-        for results_group in self._results_context.gather_results(population, event_name):
-            self._metrics.update(results_group)
+        ...
+        # population = self._prepare_population(event)
+        # for results_group in self._results_context.gather_results(population, event_name):
+        #     self._metrics.update(results_group)
 
     def set_default_stratifications(self, default_stratifications: List[str]):
         self._results_context.set_default_stratifications(default_stratifications)
@@ -76,8 +83,8 @@ class ResultsManager:
         requires_columns: List[str] = (),
         requires_values: List[str] = (),
     ) -> None:
-        self._add_resources(requires_columns, 'column')
-        self._add_resources(requires_values, 'value')
+        self._add_resources(requires_columns, "column")
+        self._add_resources(requires_values, "value")
         target_columns = list(requires_columns) + list(requires_values)
         self._results_context.add_stratification(
             name, target_columns, categories, mapper, is_vectorized
@@ -96,8 +103,10 @@ class ResultsManager:
             return pd.cut(data, bins, labels=labels, **cut_kwargs)
 
         target_arg = "required_columns" if target_type == "column" else "required_values"
-        target_kwargs = {target_arg : [target]}
-        self.register_stratification(binned_column, bins, _bin_data, is_vectorized=True, **target_kwargs)
+        target_kwargs = {target_arg: [target]}
+        self.register_stratification(
+            binned_column, bins, _bin_data, is_vectorized=True, **target_kwargs
+        )
 
     def register_observation(
         self,
@@ -108,32 +117,34 @@ class ResultsManager:
         requires_values: List[str] = None,
         additional_stratifications: List[str] = (),
         excluded_stratifications: List[str] = (),
-        when: str = 'collect_metrics',
+        when: str = "collect_metrics",
     ) -> None:
-        self._add_resources(requires_columns, 'column')
-        self._add_resources(requires_values, 'value')
+        self._add_resources(requires_columns, "column")
+        self._add_resources(requires_values, "value")
         self._results_context.add_observation(
             name,
             pop_filter,
             aggregator,
             additional_stratifications,
             excluded_stratifications,
-            when
+            when,
         )
 
     def _add_resources(self, target: List[str], target_type: str):
-        target = set(target) - {'event_time', 'current_time', 'step_size'}
-        if target_type == 'column':
+        target = set(target) - {"event_time", "current_time", "step_size"}
+        if target_type == "column":
             self._required_columns.update(target)
-        elif target_type == 'value':
+        elif target_type == "value":
             self._required_values.update(self.get_value(target))
 
     def _prepare_population(self, event: Event):
 
-        population = self.population_view.subview(list(self._required_columns)).get(event.index)
-        population['current_time'] = self.clock()
-        population['step_size'] = event.step_size
-        population['event_time'] = self.clock() + event.step_size
+        population = self.population_view.subview(list(self._required_columns)).get(
+            event.index
+        )
+        population["current_time"] = self.clock()
+        population["step_size"] = event.step_size
+        population["event_time"] = self.clock() + event.step_size
         for k, v in event.user_data.items():
             population[k] = v
         for pipeline in self._required_values:
@@ -162,7 +173,6 @@ class ResultsInterface:
     of the simulation state into representations that aren't needed for
     modeling, but are required for the stratification of produced results.
 
-
     The purpose of this interface is to provide controlled access to a results
     backend by means of the builder object. It exposes methods
     to register stratifications, set default stratifications, and register
@@ -181,6 +191,12 @@ class ResultsInterface:
 
     def __init__(self, manager: ResultsManager) -> None:
         self._manager = manager
+        self._name = "results_interface"
+
+    @property
+    def name(self) -> str:
+        """The name of this ResultsInterface."""
+        return self._name
 
     def set_default_stratifications(self, default_stratifications: List[str]):
         self._manager.set_default_stratifications(default_stratifications)
@@ -227,8 +243,8 @@ class ResultsInterface:
         binned_column: str,
         bins: List = (),
         labels: List[str] = (),
-        target_type: str = 'column',
-        **cut_kwargs
+        target_type: str = "column",
+        **cut_kwargs,
     ) -> None:
 
         self._manager.register_binned_stratification(...)
@@ -236,12 +252,12 @@ class ResultsInterface:
     def register_observation(
         self,
         name: str,
-        pop_filter: str = '',
+        pop_filter: str = "",
         aggregator: Callable[[pd.DataFrame], float] = len,
         requires_columns: List[str] = None,
         requires_values: List[str] = None,
         additional_stratifications: List[str] = (),
         excluded_stratifications: List[str] = (),
-        when: str = 'collect_metrics'
+        when: str = "collect_metrics",
     ) -> None:
         self._manager.register_observation(...)
