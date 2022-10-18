79728df8a6f18c001e792ca393ea8ce68f006694
Matthew Kappel <mkappel@uw.edu>
2022-10-18 10:55:15 -0700

    Add functionality for binned stratifications (#232)
    - *Category*: POC
    - *JIRA issue*: [MIC-3134](https://jira.ihme.washington.edu/browse/MIC-3134)
    
    Changes
    - Adds implementation of interface and manager methods for binned stratifications and docstrings
    - Adds test for registration (using `assert_called_once` as `assert_called_with` isn't available, as the mapper is defined at runtime)
    
    Testing
    My new tests work.

--- tests/framework/results/test_manager.py
+++ tests/framework/results/test_manager.py
@@ -5,6 +5,10 @@ import pytest
 from vivarium.framework.results.manager import ResultsManager
 
 from .mocks import (
+    BIN_BINNED_COLUMN,
+    BIN_LABELS,
+    BIN_SILLY_BINS,
+    BIN_SOURCE,
     CATEGORIES,
     NAME,
     SOURCES,
@@ -17,6 +21,11 @@ from .mocks import (
 # Mock for get_value call for Pipelines, returns a str instead of a Pipeline
 def mock_get_value(self, name: str):
     return name
+
+
+#######################################
+# Tests for `register_stratification` #
+#######################################
 
 
 @pytest.mark.parametrize(
@@ -155,3 +164,32 @@ def test_duplicate_name_register_stratification(mocker):
     mgr.register_stratification(NAME, CATEGORIES, sorting_hat_serial, False, SOURCES, [])
     with pytest.raises(ValueError, match=f"Name `{NAME}` is already used"):
         mgr.register_stratification(NAME, CATEGORIES, sorting_hat_vector, True, SOURCES, [])
+
+
+##############################################
+# Tests for `register_binned_stratification` #
+##############################################
+
+
+def test_register_binned_stratification(mocker):
+    mgr = ResultsManager()
+    mock_register_stratification = mocker.patch(
+        "vivarium.framework.results.manager.ResultsManager.register_stratification"
+    )
+    mgr.register_binned_stratification(
+        BIN_SOURCE, "column", BIN_BINNED_COLUMN, BIN_SILLY_BINS, BIN_LABELS
+    )
+    mock_register_stratification.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "bins, labels",
+    [(BIN_SILLY_BINS, BIN_LABELS[2:]), (BIN_SILLY_BINS[2:], BIN_LABELS)],
+    ids=["more_bins_than_labels", "more_labels_than_bins"],
+)
+def test_register_binned_stratification_raises(bins, labels):
+    mgr = ResultsManager()
+    with pytest.raises(ValueError):
+        raise mgr.register_binned_stratification(
+            BIN_SOURCE, "column", BIN_BINNED_COLUMN, bins, labels
+        )
