4e7fa000d6e9d468d7505174740278e178852621
Steve Bachmeier <sbachmei@uw.edu>
2023-05-31 11:21:16 -0700

    Revert "pin pandas>=2; stop supporting python 3.7; fix failing doctest (#292)"
    This reverts commit 4adf099627346db626983e21ed2037219fac9238.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     install_requirements = [
         "numpy",
-        "pandas>=2.0",
+        "pandas<2.0.0",
         "pyyaml>=5.1",
         "scipy",
         "click",
