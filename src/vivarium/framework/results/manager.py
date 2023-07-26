f382be0867123d548268397e178be0d5e9e3cfaf
hussain-jafari <hussainsjafari@gmail.com>
2023-07-26 13:15:44 -0500

    black again

--- src/vivarium/framework/results/manager.py
+++ src/vivarium/framework/results/manager.py
@@ -97,7 +97,9 @@ class ResultsManager:
                 )
                 if stratification.name in stratifications
             ]
-            for complete_stratifications in itertools.product(*individual_stratification_strings):
+            for complete_stratifications in itertools.product(
+                *individual_stratification_strings
+            ):
                 key = measure_str + "_".join(complete_stratifications)
                 self._metrics[key] = 0
 
