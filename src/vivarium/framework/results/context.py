ae871b73f2a2337ad0d5f0492c57a1aaa32b114a
Patrick Nast <pnast@uw.edu>
2023-09-15 13:16:51 -0700

    handle groupby default change

--- src/vivarium/framework/results/context.py
+++ src/vivarium/framework/results/context.py
@@ -123,7 +123,7 @@ class ResultsContext:
                 if not len(list(stratifications)):  # Handle situation of no stratifications
                     pop_groups = filtered_pop.groupby(lambda _: True)
                 else:
-                    pop_groups = filtered_pop.groupby(list(stratifications))
+                    pop_groups = filtered_pop.groupby(list(stratifications), observed=False)
 
                 for measure, aggregator_sources, aggregator, additional_keys in observations:
                     if aggregator_sources:
