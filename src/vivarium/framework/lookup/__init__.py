1403848c852113d1b999cb0e51eebb840105f2c0
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-10-19 16:29:14 -0700

    refactor lookup tables into a package (#358)

---
+++ src/vivarium/framework/lookup/__init__.py
@@ -0,0 +1,6 @@
+from vivarium.framework.lookup.manager import LookupTableInterface, LookupTableManager
+from vivarium.framework.lookup.table import (
+    LookupTable,
+    LookupTableData,
+    validate_parameters,
+)
