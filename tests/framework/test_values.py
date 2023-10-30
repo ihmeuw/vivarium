134fcd8c1a5ebaba6b16dc63b6e2e4e6b32eed23
Patrick Nast <pnast@uw.edu>
2023-10-30 15:49:43 -0700

    remove space, revert changes

--- tests/framework/test_values.py
+++ tests/framework/test_values.py
@@ -16,8 +16,7 @@ from vivarium.framework.values import (
 def manager(mocker):
     manager = ValuesManager()
     builder = mocker.MagicMock()
-    builder.time.step_size = lambda: lambda: pd.Timedelta(days=2)
-    builder.time.simulant_step_sizes = lambda: lambda idx: pd.Series(
+    builder.time.simulant_step_sizes = lambda: lambda: lambda idx: pd.Series(
         [pd.Timedelta(days=3) if i % 2 == 0 else pd.Timedelta(days=5) for i in idx], index=idx
     )
     manager.setup(builder)
