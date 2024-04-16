3eb6e65a2c31e01328831f207bd00f95136b281d
Steve Bachmeier <sbachmei@uw.edu>
2024-04-16 10:35:17 -0700

    remove config_tree module

--- src/vivarium/__init__.py
+++ src/vivarium/__init__.py
@@ -13,9 +13,6 @@ from vivarium.__about__ import (
 )
 from vivarium._version import __version__
 from vivarium.component import Component
-
-# TODO [MIC-4946]: Remove ConfigTree from vivarium once deprecated
-from vivarium.config_tree import ConfigTree
 from vivarium.framework.artifact import Artifact
 from vivarium.framework.configuration import build_model_specification
 from vivarium.interface import InteractiveContext
