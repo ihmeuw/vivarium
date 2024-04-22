fc277ae485c6911434fc7eeb63a31bbd48c09973
Steve Bachmeier <sbachmei@uw.edu>
2024-04-22 10:08:19 -0700

    Update/add metrics

--- tests/framework/components/test_component.py
+++ tests/framework/components/test_component.py
@@ -1,174 +1,28 @@
-from typing import Dict, List, Optional
-
 import pandas as pd
 import pytest
 from layered_config_tree.exceptions import ConfigurationError
 
-from vivarium import Artifact, Component, InteractiveContext
+from vivarium import Artifact, InteractiveContext
 from vivarium.framework.engine import Builder
-from vivarium.framework.event import Event
 from vivarium.framework.lookup.table import ScalarTable
-from vivarium.framework.population import SimulantData
-
-
-class ColumnCreator(Component):
-    @property
-    def columns_created(self) -> List[str]:
-        return ["test_column_1", "test_column_2", "test_column_3"]
-
-    def setup(self, builder: Builder) -> None:
-        builder.value.register_value_producer("pipeline_1", lambda x: x)
-        builder.randomness.get_stream("stream_1")
-
-    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
-        initialization_data = pd.DataFrame(
-            {column: 9 for column in self.columns_created}, index=pop_data.index
-        )
-        self.population_view.update(initialization_data)
-
-
-class LookupCreator(ColumnCreator):
-    CONFIGURATION_DEFAULTS = {
-        "lookup_creator": {
-            "data_sources": {
-                "favorite_team": "simulants.favorite_team",
-                "favorite_scalar": 0.4,
-                "favorite_color": "simulants.favorite_color",
-                "favorite_number": "simulants.favorite_number",
-                "baking_time": "self::load_baking_time",
-                "cooling_time": "tests.framework.components.test_component::load_cooling_time",
-            },
-            "alternate_data_sources": {
-                "favorite_list": [9, 4],
-            },
-        }
-    }
-
-    def build_all_lookup_tables(self, builder: "Builder") -> None:
-        super().build_all_lookup_tables(builder)
-        self.lookup_tables["favorite_list"] = self.build_lookup_table(
-            builder,
-            self.configuration["alternate_data_sources"]["favorite_list"],
-            ["column_1", "column_2"],
-        )
-
-    @staticmethod
-    def load_baking_time(_builder: Builder) -> float:
-        return 0.5
+from ...helper_components import (
+    AllColumnsRequirer,
+    ColumnCreator,
+    ColumnCreatorAndRequirer,
+    ColumnRequirer,
+    CustomPriorities,
+    DefaultPriorities,
+    FilteredPopulationView,
+    LookupCreator,
+    NoPopulationView,
+    Parameterized,
+    ParameterizedByComponent,
+    SingleLookupCreator,
+)
 
 
 def load_cooling_time(builder: Builder) -> pd.DataFrame:
     return builder.data.load("cooling.time")
-
-
-class SingleLookupCreator(ColumnCreator):
-    @property
-    def standard_lookup_tables(self) -> List[str]:
-        return ["favorite_color"]
-
-
-class ColumnRequirer(Component):
-    @property
-    def columns_required(self) -> List[str]:
-        return ["test_column_1", "test_column_2"]
-
-
-class ColumnCreatorAndRequirer(Component):
-    @property
-    def columns_required(self) -> List[str]:
-        return ["test_column_1", "test_column_2"]
-
-    @property
-    def columns_created(self) -> List[str]:
-        return ["test_column_4"]
-
-    @property
-    def initialization_requirements(self) -> Dict[str, List[str]]:
-        return {
-            "requires_columns": ["test_column_2"],
-            "requires_values": ["pipeline_1"],
-            "requires_streams": ["stream_1"],
-        }
-
-    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
-        initialization_data = pd.DataFrame({"test_column_4": 8}, index=pop_data.index)
-        self.population_view.update(initialization_data)
-
-
-class AllColumnsRequirer(Component):
-    @property
-    def columns_required(self) -> List[str]:
-        return []
-
-
-class FilteredPopulationView(ColumnRequirer):
-    @property
-    def population_view_query(self) -> Optional[str]:
-        return "test_column_1 == 5"
-
-
-class NoPopulationView(Component):
-    pass
-
-
-class Parameterized(Component):
-    def __init__(self, p_one: str, p_two: int, p_three: str):
-        super().__init__()
-        self.p_one = p_one
-        self.p_two = p_two
-        self._p_three = p_three
-
-
-class ParameterizedByComponent(Component):
-    def __init__(self, param: Parameterized):
-        super().__init__()
-        self.param = param
-
-
-class DefaultPriorities(Component):
-    def on_post_setup(self, event: Event) -> None:
-        pass
-
-    def on_time_step_prepare(self, event: Event) -> None:
-        pass
-
-    def on_time_step(self, event: Event) -> None:
-        pass
-
-    def on_time_step_cleanup(self, event: Event) -> None:
-        pass
-
-    def on_collect_metrics(self, event: Event) -> None:
-        pass
-
-    def on_simulation_end(self, event: Event) -> None:
-        pass
-
-
-class CustomPriorities(DefaultPriorities):
-    @property
-    def post_setup_priority(self) -> int:
-        return 8
-
-    @property
-    def time_step_prepare_priority(self) -> int:
-        return 7
-
-    @property
-    def time_step_priority(self) -> int:
-        return 2
-
-    @property
-    def time_step_cleanup_priority(self) -> int:
-        return 3
-
-    @property
-    def collect_metrics_priority(self) -> int:
-        return 6
-
-    @property
-    def simulation_end_priority(self) -> int:
-        return 1
 
 
 def test_unique_component_has_correct_repr():
