d46f9e0097e9661d9feb062e5fcd7531686e9762
Steve Bachmeier <sbachmei@uw.edu>
2024-04-25 16:46:59 -0700

    break up gather_results into sub-functions

--- src/vivarium/framework/results/context.py
+++ src/vivarium/framework/results/context.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
-from typing import Callable, Generator, List, Optional, Tuple
+from typing import Callable, Generator, List, Optional, Tuple, Union
 
 import pandas as pd
+from pandas.core.groupby import DataFrameGroupBy
 
 from vivarium.framework.results.exceptions import ResultsConfigurationError
 from vivarium.framework.results.stratification import Stratification
@@ -114,55 +115,23 @@ class ResultsContext:
         ].items():
             # Results production can be simplified to
             # filter -> groupby -> aggregate in all situations we've seen.
-            if pop_filter:
-                filtered_pop = population.query(pop_filter)
-            else:
-                filtered_pop = population
+            filtered_pop = self._filter_population(population, pop_filter)
             if filtered_pop.empty:
                 yield None, None
             else:
-                if not list(stratifications):
-                    # We do not want to stratify, i.e. aggregate the entire population
-                    pop_groups = filtered_pop
-                else:
-                    pop_groups = filtered_pop.groupby(list(stratifications), observed=False)
+                pop_groups = self._get_groups(stratifications, filtered_pop)
 
                 for measure, aggregator_sources, aggregator, _additional_keys in observations:
-                    if aggregator_sources:
-                        aggregates = (
-                            pop_groups[aggregator_sources].apply(aggregator).fillna(0.0)
-                        )
-                    else:
-                        # aggregate all columns
-                        if (
-                            isinstance(pop_groups, pd.DataFrame)
-                            and len(pop_groups.columns) > 1
-                        ):
-                            # Apply the aggregator to the entire group
-                            aggregates = aggregator(pop_groups)
-                            # convert the result to a DataFrame
-                            aggregates = pd.DataFrame(data=[aggregates])
-                        else:
-                            aggregates = pop_groups.apply(aggregator)
-
-                    # Ensure we are dealing with a single column of formattable results):
-                    if isinstance(aggregates, pd.Series):
-                        aggregates = pd.DataFrame(aggregates)
+                    aggregates = self._aggregate(pop_groups, aggregator_sources, aggregator)
+                    # NOTE: We only support metrics of type pd.DataFrame w/ a single column
+                    aggregates = self._coerce_to_dataframe(aggregates)
                     if aggregates.shape[1] != 1:
                         raise TypeError(
                             f"The aggregator return value has {aggregates.shape[1]} columns "
                             "while a single column is expected."
                         )
-
-                    # fill missing index levels with 0s
-                    if isinstance(aggregates.index, pd.MultiIndex):
-                        full_idx = pd.MultiIndex.from_product(aggregates.index.levels)
-                    else:
-                        full_idx = aggregates.index
-                    aggregates = aggregates.reindex(full_idx).fillna(0.0)
-
+                    aggregates = self._expand_index(aggregates)
                     aggregates.rename(columns={aggregates.columns[0]: "value"}, inplace=True)
-
                     # When no stratifications, ensure the index name is the measure
                     # and not just the aggregator_sources
                     if not list(stratifications):
@@ -180,3 +149,50 @@ class ResultsContext:
         )
         # Makes sure measure identifiers have fields in the same relative order.
         return tuple(sorted(stratifications))
+
+    @staticmethod
+    def _filter_population(population: pd.DataFrame, pop_filter: str) -> pd.DataFrame:
+        return population.query(pop_filter) if pop_filter else population
+
+    @staticmethod
+    def _get_groups(
+        stratifications: Tuple[Optional[str]], filtered_pop: pd.DataFrame
+    ) -> Union[DataFrameGroupBy, pd.DataFrame]:
+        if not list(stratifications):
+            # We do not want to stratify, i.e. aggregate the entire population
+            pop_groups = filtered_pop
+        else:
+            pop_groups = filtered_pop.groupby(list(stratifications), observed=False)
+        return pop_groups
+
+    @staticmethod
+    def _aggregate(
+        pop_groups: Union[DataFrameGroupBy, pd.DataFrame],
+        aggregator_sources: Optional[List[str]],
+        aggregator: Callable[[pd.DataFrame], float],
+    ) -> Union[pd.Series, pd.DataFrame]:
+        if aggregator_sources:
+            aggregates = pop_groups[aggregator_sources].apply(aggregator).fillna(0.0)
+        else:
+            # aggregate all columns
+            if isinstance(pop_groups, pd.DataFrame) and len(pop_groups.columns) > 1:
+                # Apply the aggregator to the entire group
+                aggregates = aggregator(pop_groups)
+                # convert the result to a DataFrame
+                aggregates = pd.DataFrame(data=[aggregates])
+            else:
+                aggregates = pop_groups.apply(aggregator)
+        return aggregates
+
+    @staticmethod
+    def _coerce_to_dataframe(aggregates: Union[pd.Series, pd.DataFrame]) -> pd.DataFrame:
+        return aggregates.to_frame() if isinstance(aggregates, pd.Series) else aggregates
+
+    @staticmethod
+    def _expand_index(aggregates: pd.DataFrame) -> pd.DataFrame:
+        if isinstance(aggregates.index, pd.MultiIndex):
+            full_idx = pd.MultiIndex.from_product(aggregates.index.levels)
+        else:
+            full_idx = aggregates.index
+        aggregates = aggregates.reindex(full_idx).fillna(0.0)
+        return aggregates
