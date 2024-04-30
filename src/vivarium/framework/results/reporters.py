e318220df7aa8045d23e0ab1fc96bda0a4337473
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-04-30 12:44:08 -0600

    create Observation dataclass (#404)

--- src/vivarium/framework/results/reporters.py
+++ src/vivarium/framework/results/reporters.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 
 def dataframe_to_csv(
-    results_dir: Path, measure: str, df: pd.DataFrame, random_seed: str, input_draw: str
+    results_dir: Path, measure: str, results: pd.DataFrame, random_seed: str, input_draw: str
 ) -> None:
     # Add extra cols
-    df[["measure"]] = measure
-    df["random_seed"] = random_seed
-    df["input_draw"] = input_draw
+    results[["measure"]] = measure
+    results["random_seed"] = random_seed
+    results["input_draw"] = input_draw
     # Sort the columns such that the stratifications (index) are first
     # and "value" is last and sort the rows by the stratifications.
-    other_cols = [c for c in df.columns if c != "value"]
-    df = df[other_cols + ["value"]].sort_index().reset_index()
-    df.to_csv(results_dir / f"{measure}.csv", index=False)
+    other_cols = [c for c in results.columns if c != "value"]
+    results = results[other_cols + ["value"]].sort_index().reset_index()
+    results.to_csv(results_dir / f"{measure}.csv", index=False)
