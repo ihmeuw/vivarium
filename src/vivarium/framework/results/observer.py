459b6bc1b75c981ed4c29426c315fedee8e2f7a2
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-05-08 13:53:51 -0600

    use a constant for metrics column 'value' (#410)

--- src/vivarium/framework/results/observer.py
+++ src/vivarium/framework/results/observer.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from vivarium import Component
 from vivarium.framework.engine import Builder
+from vivarium.framework.results import METRICS_COLUMN
 
 
 class Observer(Component, ABC):
@@ -58,9 +59,9 @@ class Observer(Component, ABC):
         results["random_seed"] = self.random_seed
         results["input_draw"] = self.input_draw
         # Sort the columns such that the stratifications (index) are first
-        # and "value" is last and sort the rows by the stratifications.
-        other_cols = [c for c in results.columns if c != "value"]
-        results = results[other_cols + ["value"]].sort_index().reset_index()
+        # and METRICS_COLUMN is last and sort the rows by the stratifications.
+        other_cols = [c for c in results.columns if c != METRICS_COLUMN]
+        results = results[other_cols + [METRICS_COLUMN]].sort_index().reset_index()
         results.to_csv(Path(self.results_dir) / f"{measure}.csv", index=False)
 
 
