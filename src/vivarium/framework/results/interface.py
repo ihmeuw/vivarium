3fe91270f52ebe2bb5903cca793fc6dbdcfe95af
Steve Bachmeier <sbachmei@uw.edu>
2024-05-10 09:19:21 -0700

    fix report default

--- src/vivarium/framework/results/interface.py
+++ src/vivarium/framework/results/interface.py
@@ -143,7 +143,7 @@ class ResultsInterface:
         additional_stratifications: List[str] = [],
         excluded_stratifications: List[str] = [],
         when: str = "collect_metrics",
-        report: Callable[[str, pd.DataFrame], None] = lambda *_: None,
+        report: Callable[[str, pd.DataFrame], None] = lambda measure, results: None,
     ) -> None:
         """Provide the results system all the information it needs to perform the observation.
 
