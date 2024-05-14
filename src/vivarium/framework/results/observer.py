e9b0a354669e7e3a51567ad67e0fa702ffc5a216
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-05-14 14:30:25 -0600

    Feature/sbachmei/remove observation report method (#414)

--- src/vivarium/framework/results/observer.py
+++ src/vivarium/framework/results/observer.py
@@ -1,12 +1,8 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
 from typing import Any, Dict
-
-import pandas as pd
 
 from vivarium import Component
 from vivarium.framework.engine import Builder
-from vivarium.framework.results import METRICS_COLUMN
 
 
 class Observer(Component, ABC):
@@ -48,21 +44,6 @@ class Observer(Component, ABC):
         self.random_seed = (
             builder.configuration.to_dict().get("randomness", {}).get("random_seed", None)
         )
-
-    ##################
-    # Report methods #
-    ##################
-
-    def dataframe_to_csv(self, measure: str, results: pd.DataFrame) -> None:
-        # Add extra cols
-        results["measure"] = measure
-        results["random_seed"] = self.random_seed
-        results["input_draw"] = self.input_draw
-        # Sort the columns such that the stratifications (index) are first
-        # and METRICS_COLUMN is last and sort the rows by the stratifications.
-        other_cols = [c for c in results.columns if c != METRICS_COLUMN]
-        results = results[other_cols + [METRICS_COLUMN]].sort_index().reset_index()
-        results.to_csv(Path(self.results_dir) / f"{measure}.csv", index=False)
 
 
 class StratifiedObserver(Observer):
