a03b92150fea688cd522f395cf0519bbb0adf43f
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2024-06-04 14:03:34 -0600

    add cli 'simulate run' test (#428)

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+from tests.framework.results.helpers import (
+    HARRY_POTTER_CONFIG,
+    Hogwarts,
+    HogwartsResultsStratifier,
+    HousePointsObserver,
+    NoStratificationsQuidditchWinsObserver,
+    QuidditchWinsObserver,
+)
