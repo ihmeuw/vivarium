5cd1ee13d0f2116359908a7bba14d45ea697a1d8
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-09-03 15:43:13 -0700

    fix errors in types.py

--- src/vivarium/types.py
+++ src/vivarium/types.py
@@ -3,10 +3,18 @@ from numbers import Number
 from typing import Union
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 
 ScalarValue = Union[Number, timedelta, datetime]
 LookupTableData = Union[ScalarValue, pd.DataFrame, list[ScalarValue], tuple[ScalarValue]]
-NumberLike = Union[np.ndarray, pd.Series, pd.DataFrame, Number]
+# FIXME: For some of the uses of NumberLike, we probably want a TypeVar here instead.
+NumberLike = Union[
+    npt.NDArray[np.number[npt.NBitBase]],
+    # FIXME: We don't want to ignore this, but the code fails if we parameterize pd.Series.
+    pd.Series,  # type: ignore [type-arg]
+    pd.DataFrame,
+    Number,
+]
 Time = Union[pd.Timestamp, datetime, Number]
 Timedelta = Union[pd.Timedelta, timedelta, Number]
