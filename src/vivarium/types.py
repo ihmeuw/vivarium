a0ff42812c2c79f54a8635bf4391ba46dabbf8d5
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-09-06 15:00:27 -0700

    exclude failing files from mypy checking

--- src/vivarium/types.py
+++ src/vivarium/types.py
@@ -8,13 +8,14 @@ import pandas as pd
 
 ScalarValue = Union[Number, timedelta, datetime]
 LookupTableData = Union[ScalarValue, pd.DataFrame, list[ScalarValue], tuple[ScalarValue]]
-# FIXME: For some of the uses of NumberLike, we probably want a TypeVar here instead.
+# TODO: For some of the uses of NumberLike, we probably want a TypeVar here instead.
 NumberLike = Union[
     npt.NDArray[np.number[npt.NBitBase]],
-    # FIXME: We don't want to ignore this, but the code fails if we parameterize pd.Series.
+    # TODO: Parameterizing pandas objects fails below python 3.12
     pd.Series,  # type: ignore [type-arg]
     pd.DataFrame,
     Number,
 ]
+# todo need to use TypeVars here
 Time = Union[pd.Timestamp, datetime, Number]
 Timedelta = Union[pd.Timedelta, timedelta, Number]
--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from tests.framework.results.helpers import (
-    HARRY_POTTER_CONFIG,
-    Hogwarts,
-    HogwartsResultsStratifier,
-    HousePointsObserver,
-    NoStratificationsQuidditchWinsObserver,
-    QuidditchWinsObserver,
-)
