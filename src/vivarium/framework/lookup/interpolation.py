69d260cfbf001d2f86c80d0e0392c3810988ab1c
Hussain Jafari <hussainsjafari@gmail.com>
2024-10-10 10:15:53 -0700

    working type

--- src/vivarium/framework/lookup/interpolation.py
+++ src/vivarium/framework/lookup/interpolation.py
@@ -15,7 +15,7 @@ from typing import Union
 import numpy as np
 import pandas as pd
 
-_SubTablesType = list[tuple[tuple[Hashable, ...] | Hashable | None, pd.DataFrame]]
+_SubTablesType = list[tuple[Union[tuple[Hashable, ...], Hashable, None], pd.DataFrame]]
 
 
 class Interpolation:
