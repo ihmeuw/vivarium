c8b9201202b067e9440aee779dbdc50eb32a7712
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-10-17 10:35:04 -0700

    specify required resources in example models (#507)

--- src/vivarium/examples/boids/neighbors.py
+++ src/vivarium/examples/boids/neighbors.py
@@ -26,7 +26,7 @@ class Neighbors(Component):
         self.neighbors_calculated = False
         self._neighbors = pd.Series()
         self.neighbors = builder.value.register_value_producer(
-            "neighbors", source=self.get_neighbors
+            "neighbors", source=self.get_neighbors, required_resources=self.columns_required
         )
 
     ########################
