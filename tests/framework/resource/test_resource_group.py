127488e75e77c5e41ae9843b8251fbc3aac3baa8
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-10-19 15:21:19 -0700

    refactor resource arguments of add_resources

--- tests/framework/resource/test_resource_group.py
+++ tests/framework/resource/test_resource_group.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 from datetime import datetime
 
+import pytest
+
 from vivarium.framework.randomness import RandomnessStream
 from vivarium.framework.randomness.index_map import IndexMap
+from vivarium.framework.resource.exceptions import ResourceError
 from vivarium.framework.resource.group import ResourceGroup
 from vivarium.framework.resource.resource import Column
-from vivarium.framework.values import Pipeline, ValueSource
+from vivarium.framework.values import Pipeline, ValueModifier, ValueSource
 
 
 def dummy_producer() -> str:
@@ -14,9 +17,7 @@ def dummy_producer() -> str:
 
 
 def test_resource_group() -> None:
-    r_type = "column"
-    r_names = [str(i) for i in range(5)]
-    r_producer = dummy_producer
+    resources = [ValueModifier(str(i)) for i in range(5)]
     r_dependencies = [
         Column("an_interesting_column"),
         Pipeline("baz"),
@@ -24,10 +25,10 @@ def test_resource_group() -> None:
         ValueSource("foo"),
     ]
 
-    rg = ResourceGroup(r_type, r_names, r_producer, r_dependencies)
+    rg = ResourceGroup(resources, dummy_producer, r_dependencies)
 
-    assert rg.type == r_type
-    assert rg.names == [f"{r_type}.{name}" for name in r_names]
+    assert rg.type == "value_modifier"
+    assert rg.names == [f"value_modifier.{i}" for i in range(5)]
     assert rg.producer == dummy_producer
     assert rg.dependencies == [
         "column.an_interesting_column",
@@ -36,3 +37,15 @@ def test_resource_group() -> None:
         "value_source.foo",
     ]
     assert list(rg) == rg.names
+
+
+def test_resource_group_with_no_resources() -> None:
+    with pytest.raises(ResourceError, match="must have at least one resource"):
+        _ = ResourceGroup([], dummy_producer, [Column("foo")])
+
+
+def test_resource_group_with_multiple_resource_types() -> None:
+    resources = [ValueModifier("foo"), ValueSource("bar")]
+
+    with pytest.raises(ResourceError, match="resources must be of the same type"):
+        _ = ResourceGroup(resources, dummy_producer)
