aeba03e10759d8b37dc72b3afb7a12e5029e8606
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-10-21 13:59:47 -0700

    create resource types (#511)

--- tests/framework/resource/test_resource_group.py
+++ tests/framework/resource/test_resource_group.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-import pytest
+from datetime import datetime
 
 from vivarium.framework.randomness import RandomnessStream
 from vivarium.framework.randomness.index_map import IndexMap
-from vivarium.framework.resource import Resource
-from vivarium.framework.resource.exceptions import ResourceError
 from vivarium.framework.resource.group import ResourceGroup
-from vivarium.framework.values import Pipeline
+from vivarium.framework.resource.resource import Column
+from vivarium.framework.values import Pipeline, ValueSource
 
 
 def dummy_producer() -> str:
@@ -18,11 +17,11 @@ def test_resource_group() -> None:
     r_type = "column"
     r_names = [str(i) for i in range(5)]
     r_producer = dummy_producer
-    r_dependencies: list[str | Pipeline | RandomnessStream | Resource] = [
-        "an_interesting_column",
+    r_dependencies = [
+        Column("an_interesting_column"),
         Pipeline("baz"),
-        RandomnessStream("bar", lambda x: x, 1, IndexMap()),
-        Resource("value_source", "foo"),
+        RandomnessStream("bar", lambda: datetime.now(), 1, IndexMap()),
+        ValueSource("foo"),
     ]
 
     rg = ResourceGroup(r_type, r_names, r_producer, r_dependencies)
@@ -37,24 +36,3 @@ def test_resource_group() -> None:
         "value_source.foo",
     ]
     assert list(rg) == rg.names
-
-
-@pytest.mark.parametrize(
-    "dependency, expected_key",
-    [
-        ("an_interesting_column", "column.an_interesting_column"),
-        (Pipeline("baz"), "value.baz"),
-        (RandomnessStream("bar", lambda x: x, 1, IndexMap()), "stream.bar"),
-        (Resource("value_source", "foo"), "value_source.foo"),
-    ],
-)
-def test__get_dependency_key(
-    dependency: str | Pipeline | RandomnessStream | Resource, expected_key: str
-) -> None:
-    key = ResourceGroup._get_dependency_key(dependency)
-    assert key == expected_key
-
-
-def test__get_dependency_key_unknown_type() -> None:
-    with pytest.raises(ResourceError, match="unknown type"):
-        ResourceGroup._get_dependency_key(1)  # type: ignore [arg-type]
