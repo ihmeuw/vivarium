4c30ff6c410f1731aa7284ffe0a0b176c2608178
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-10-21 14:44:35 -0700

    improve distinction between initializers and non-initializers (#513)

--- tests/framework/resource/test_resource_group.py
+++ tests/framework/resource/test_resource_group.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import pytest
 
+from vivarium.framework.population import SimulantData
 from vivarium.framework.randomness import RandomnessStream
 from vivarium.framework.randomness.index_map import IndexMap
 from vivarium.framework.resource.exceptions import ResourceError
@@ -12,12 +13,12 @@ from vivarium.framework.resource.resource import Column
 from vivarium.framework.values import Pipeline, ValueModifier, ValueSource
 
 
-def dummy_producer() -> str:
-    return "resources!"
+def dummy_initializer(_simulant_data: SimulantData) -> None:
+    pass
 
 
 def test_resource_group() -> None:
-    resources = [ValueModifier(str(i)) for i in range(5)]
+    resources = [Column(str(i)) for i in range(5)]
     r_dependencies = [
         Column("an_interesting_column"),
         Pipeline("baz"),
@@ -25,11 +26,11 @@ def test_resource_group() -> None:
         ValueSource("foo"),
     ]
 
-    rg = ResourceGroup(resources, dummy_producer, r_dependencies)
+    rg = ResourceGroup(resources, r_dependencies, dummy_initializer)
 
-    assert rg.type == "value_modifier"
-    assert rg.names == [f"value_modifier.{i}" for i in range(5)]
-    assert rg.producer == dummy_producer
+    assert rg.type == "column"
+    assert rg.names == [f"column.{i}" for i in range(5)]
+    assert rg.initializer == dummy_initializer
     assert rg.dependencies == [
         "column.an_interesting_column",
         "value.baz",
@@ -39,13 +40,21 @@ def test_resource_group() -> None:
     assert list(rg) == rg.names
 
 
+def test_resource_group_is_initializer() -> None:
+    resources = [ValueModifier("foo")]
+    rg = ResourceGroup(resources, [Column("bar")])
+
+    with pytest.raises(ResourceError, match="does not have an initializer"):
+        _ = rg.initializer
+
+
 def test_resource_group_with_no_resources() -> None:
     with pytest.raises(ResourceError, match="must have at least one resource"):
-        _ = ResourceGroup([], dummy_producer, [Column("foo")])
+        _ = ResourceGroup([], [Column("foo")])
 
 
 def test_resource_group_with_multiple_resource_types() -> None:
     resources = [ValueModifier("foo"), ValueSource("bar")]
 
     with pytest.raises(ResourceError, match="resources must be of the same type"):
-        _ = ResourceGroup(resources, dummy_producer)
+        _ = ResourceGroup(resources, [])
