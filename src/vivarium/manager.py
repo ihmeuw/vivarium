b32fea5072cbd04e12c4db630845f6fe644b2983
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2024-10-31 17:16:49 -0700

    wire up managers to be able to create columns

--- src/vivarium/manager.py
+++ src/vivarium/manager.py
@@ -7,15 +7,18 @@ A base Manager class to be used to create manager for use in ``vivarium``
 simulations.
 
 """
+from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from vivarium.framework.engine import Builder
+    from vivarium.framework.population import SimulantData
 
 
-class Manager:
-    CONFIGURATION_DEFAULTS: Dict[str, Any] = {}
+class Manager(ABC):
+    CONFIGURATION_DEFAULTS: dict[str, Any] = {}
     """A dictionary containing the defaults for any configurations managed by this
     manager. An empty dictionary indicates no managed configurations.
 
@@ -26,25 +29,57 @@ class Manager:
     ##############
 
     @property
-    def configuration_defaults(self) -> Dict[str, Any]:
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    def configuration_defaults(self) -> dict[str, Any]:
         """Provides a dictionary containing the defaults for any configurations
         managed by this manager.
 
         These default values will be stored at the `component_configs` layer of the
         simulation's LayeredConfigTree.
-
-        Returns
-        -------
-            A dictionary containing the defaults for any configurations managed by
-            this manager.
         """
         return self.CONFIGURATION_DEFAULTS
+
+    @property
+    def columns_created(self) -> list[str]:
+        """Provides names of columns created by the manager."""
+        return []
 
     #####################
     # Lifecycle methods #
     #####################
 
-    def setup(self, builder: "Builder") -> None:
+    def setup(self, builder: Builder) -> None:
+        """Defines custom actions this manager needs to run during the setup
+        lifecycle phase.
+
+        This method is intended to be overridden by subclasses to perform any
+        necessary setup operations specific to the manager. By default, it
+        does nothing.
+
+        Parameters
+        ----------
+        builder
+            The builder object used to set up the manager.
+        """
+        pass
+
+    def on_initialize_simulants(self, pop_data: SimulantData) -> None:
+        """
+        Method that vivarium will run during simulant initialization.
+
+        This method is intended to be overridden by subclasses if there are
+        operations they need to perform specifically during the simulant
+        initialization phase.
+
+        Parameters
+        ----------
+        pop_data : SimulantData
+            The data associated with the simulants being initialized.
+        """
         pass
 
 
