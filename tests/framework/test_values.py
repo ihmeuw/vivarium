e9032b8090d9150a31ac8431dbd9cb04694de165
Steve Bachmeier <sbachmei@uw.edu>
2025-10-24 12:01:29 -0600

    tweak error message

--- tests/framework/test_values.py
+++ tests/framework/test_values.py
@@ -511,7 +511,7 @@ def test_source_callable(
             TypeError,
             match=(
                 "The source of an attribute pipeline must be a callable or a list "
-                f"of private column names, but got {type(source)}."
+                f"of source column names, but got {type(source)}."
             ),
         ):
             pl(INDEX)
