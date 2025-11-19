9b9a87b1d8e32147d42a7709a8dd664117323fc7
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2025-11-19 12:29:49 -0700

    Fix typo in docstring for query parameter

--- src/vivarium/framework/population/manager.py
+++ src/vivarium/framework/population/manager.py
@@ -292,7 +292,7 @@ class PopulationManager(Manager):
             A filter on the population state.  This filters out particular
             simulants (rows in the state table) based on their current state.
             The query should be provided in a way that is understood by the
-            :meth:`pandas.DataFrame.query` method and may reference any attrubytes
+            :meth:`pandas.DataFrame.query` method and may reference any attributes
             (not just created by the ``component``).
 
         Returns
