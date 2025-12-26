478eec42b5b7f895f6ae3630f5e4d799c3291619
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2025-12-26 16:45:47 -0500

    require component to be passed in when creating resources (#712)

--- src/vivarium/examples/disease_model/mortality.py
+++ src/vivarium/examples/disease_model/mortality.py
@@ -48,7 +48,7 @@ class Mortality(Component):
         builder
             Access to simulation tools and subsystems.
         """
-        self.randomness = builder.randomness.get_stream("mortality")
+        self.randomness = builder.randomness.get_stream("mortality", self)
         builder.value.register_rate_producer(
             "mortality_rate", source=self.build_lookup_table(builder, "mortality_rate"), component=self
         )
