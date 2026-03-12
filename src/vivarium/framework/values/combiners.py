ab5a03d405ce38be61d4f837f5a6f10d1c67c519
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2026-03-12 19:28:35 -0400

    create multiplication and addition combiners (#768)

--- src/vivarium/framework/values/combiners.py
+++ src/vivarium/framework/values/combiners.py
@@ -1,6 +1,8 @@
 from collections.abc import Callable
 from typing import Any, Protocol
 
+from vivarium.types import NumberLike
+
 
 class ValueCombiner(Protocol):
     def __call__(
@@ -62,3 +64,57 @@ def list_combiner(
     """
     value.append(mutator(*args, **kwargs))
     return value
+
+
+def multiplication_combiner(
+    value: NumberLike, mutator: Callable[..., NumberLike], *args: Any, **kwargs: Any
+) -> NumberLike:
+    """Multiplies the previous pipeline output with the output of the mutator.
+
+    This combiner is meant to be used when the pipeline's final value is
+    the product of all intermediate values.
+
+    Parameters
+    ----------
+    value
+        The value from the previous step in the pipeline.
+    mutator
+        A callable that takes in all arguments that the pipeline source takes
+        in plus an additional last positional argument for the value from
+        the previous stage in the pipeline.
+    args, kwargs
+        The same args and kwargs provided during the invocation of the
+        pipeline.
+
+    Returns
+    -------
+        A modified version of the input value.
+    """
+    return value * mutator(*args, **kwargs)
+
+
+def addition_combiner(
+    value: NumberLike, mutator: Callable[..., NumberLike], *args: Any, **kwargs: Any
+) -> NumberLike:
+    """Adds the previous pipeline output with the output of the mutator.
+
+    This combiner is meant to be used when the pipeline's final value is
+    the sum of all intermediate values.
+
+    Parameters
+    ----------
+    value
+        The value from the previous step in the pipeline.
+    mutator
+        A callable that takes in all arguments that the pipeline source takes
+        in plus an additional last positional argument for the value from
+        the previous stage in the pipeline.
+    args, kwargs
+        The same args and kwargs provided during the invocation of the
+        pipeline.
+
+    Returns
+    -------
+        A modified version of the input value.
+    """
+    return value + mutator(*args, **kwargs)
